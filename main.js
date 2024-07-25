9005afbc12bee2c62ded4f4398069ffd913d6a88
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-07-25 10:22:15 +0200

    Exploration des différentes transformations que l'on peut appliquer à un Object3d

--- main.js
+++ main.js
@@ -1,17 +1,47 @@
 import * as THREE from "three";
+import "./style.css";
 
 const canvas = document.querySelector("canvas.webgl");
 
 // Scene
 const scene = new THREE.Scene();
 
-// Object
+// ---------------------------------------------
+
+// Objects
+
+const group = new THREE.Group();
+scene.add(group);
+
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
 
+// Position
+// mesh.position.y = -0.6;
+// mesh.position.x = 0.7;
+// mesh.position.z = 1;
+mesh.position.set(0.7, -0.6, 1);
+
+// Scale
+// mesh.scale.y = 2;
+// mesh.scale.x = 0.5;
+// mesh.scale.z = 0.5;
+mesh.scale.set(2, 0.5, 0.5);
+
+// Rotation
+mesh.rotation.reorder("YXZ");
+mesh.rotation.y = Math.PI * 0.25;
+mesh.rotation.x = Math.PI * 0.25;
+// mesh.rotation.z = Math.PI * 0.25;
+// mesh.rotation.set(Math.PI * 0.25, Math.PI * 0.25, Math.PI * 0.25);
+
 // Add object to scene
 scene.add(mesh);
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
 
 // Sizes
 const sizes = {
@@ -24,7 +54,12 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 
 // Position camera
 camera.position.z = 3;
+camera.position.y = 1;
+camera.position.x = 1;
 scene.add(camera);
+
+// Look at
+camera.lookAt(mesh.position);
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
