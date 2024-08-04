0527a3f42dd9271b8e09cf22de523954bfdcf2c7
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-08-04 11:29:15 +0200

    Changement de la couleur du fond du canvas

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ const canvas = document.querySelector("canvas.webgl");
 
 // Scene
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x0f0f0f);
 
 // Axes helper
 // const axesHelper = new THREE.AxesHelper();
