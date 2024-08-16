d6556827137d9480cff578cb6baa35d3d4cac39c
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-08-16 23:40:07 +0200

    Forme principale de la galaxy et ajout de debug ui

--- main.js
+++ main.js
@@ -19,23 +19,44 @@ const scene = new THREE.Scene();
  * Galaxy
  */
 const parameters = {};
-parameters.count = 1000;
-parameters.size = 0.02;
+parameters.count = 100000;
+parameters.size = 0.01;
+parameters.radius = 5;
+parameters.branches = 3;
+parameters.spin = 1;
+
+let geometry = null;
+let material = null;
+let points = null;
 
 const generateGalaxy = () => {
   /**
+   * Destroy old galaxy
+   */
+  if (points !== null) {
+    geometry.dispose();
+    material.dispose();
+    scene.remove(points);
+  }
+
+  /**
    * Geometry
    */
-  const geometry = new THREE.BufferGeometry();
+  geometry = new THREE.BufferGeometry();
 
   const positions = new Float32Array(parameters.count * 3);
 
   for (let i = 0; i < parameters.count; i++) {
     const i3 = i * 3;
 
-    positions[i3 + 0] = Math.random() - 0.5;
-    positions[i3 + 1] = Math.random() - 0.5;
-    positions[i3 + 2] = Math.random() - 0.5;
+    const radius = Math.random() * parameters.radius;
+    const branchAngle =
+      ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
+    const spinAngle = radius * parameters.spin;
+
+    positions[i3 + 0] = Math.cos(branchAngle + spinAngle) * radius;
+    positions[i3 + 1] = 0;
+    positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius;
   }
 
   geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
@@ -43,7 +64,7 @@ const generateGalaxy = () => {
   /**
    * Material
    */
-  const material = new THREE.PointsMaterial({
+  material = new THREE.PointsMaterial({
     size: parameters.size,
     sizeAttenuation: true,
     depthWrite: false,
@@ -53,11 +74,43 @@ const generateGalaxy = () => {
   /**
    * Points
    */
-  const points = new THREE.Points(geometry, material);
+  points = new THREE.Points(geometry, material);
   scene.add(points);
 };
 
 generateGalaxy();
+
+// Debug tweaks
+gui
+  .add(parameters, "count")
+  .min(100)
+  .max(100000)
+  .step(100)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "size")
+  .min(0.001)
+  .max(0.1)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "radius")
+  .min(0.1)
+  .max(20)
+  .step(0.1)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "branches")
+  .min(2)
+  .max(20)
+  .step(1)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "spin")
+  .min(-5)
+  .max(5)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
 
 /**
  * Sizes
