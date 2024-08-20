0a33c04b1220040ff466343b146546a0b290378b
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-08-20 12:52:29 +0200

    Starter pour la leçon 23

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import "./style.css";
 
 /**
  * Base
@@ -17,60 +18,51 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 /**
- * Objects
+ * Models
  */
-const object1 = new THREE.Mesh(
-  new THREE.SphereGeometry(0.5, 16, 16),
-  new THREE.MeshBasicMaterial({ color: "#ff0000" })
-);
-object1.position.x = -2;
-
-const object2 = new THREE.Mesh(
-  new THREE.SphereGeometry(0.5, 16, 16),
-  new THREE.MeshBasicMaterial({ color: "#ff0000" })
-);
-
-const object3 = new THREE.Mesh(
-  new THREE.SphereGeometry(0.5, 16, 16),
-  new THREE.MeshBasicMaterial({ color: "#ff0000" })
-);
-object3.position.x = 2;
-
-scene.add(object1, object2, object3);
-
-object1.updateMatrixWorld();
-object2.updateMatrixWorld();
-object3.updateMatrixWorld();
-
-/**
- * Model
- */
-let model = null;
-const gltfLoader = new GLTFLoader();
 const dracoLoader = new DRACOLoader();
 dracoLoader.setDecoderPath("/draco/");
+
+const gltfLoader = new GLTFLoader();
 gltfLoader.setDRACOLoader(dracoLoader);
 
-gltfLoader.load("/models/Duck/glTF-Draco/Duck.gltf", (gltf) => {
-  model = gltf.scene;
-  gltf.scene.position.y = -1.2;
+let mixer = null;
+
+gltfLoader.load("/models/hamburger.glb", (gltf) => {
   scene.add(gltf.scene);
 });
 
 /**
- * Lights
+ * Floor
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.9);
-scene.add(ambientLight);
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 2.1);
-directionalLight.position.set(1, 2, 3);
-scene.add(directionalLight);
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(50, 50),
+  new THREE.MeshStandardMaterial({
+    color: "#444444",
+    metalness: 0,
+    roughness: 0.5,
+  })
+);
+floor.receiveShadow = true;
+floor.rotation.x = -Math.PI * 0.5;
+scene.add(floor);
 
 /**
- * Raycaster
+ * Lights
  */
-const raycaster = new THREE.Raycaster();
+const ambientLight = new THREE.AmbientLight(0xffffff, 2.4);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1.8);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.camera.left = -7;
+directionalLight.shadow.camera.top = 7;
+directionalLight.shadow.camera.right = 7;
+directionalLight.shadow.camera.bottom = -7;
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);
 
 /**
  * Sizes
@@ -95,32 +87,6 @@ window.addEventListener("resize", () => {
 });
 
 /**
- * Mouse
- */
-const mouse = new THREE.Vector2();
-
-window.addEventListener("mousemove", (event) => {
-  mouse.x = (event.clientX / sizes.width) * 2 - 1;
-  mouse.y = -(event.clientY / sizes.height) * 2 + 1;
-});
-
-window.addEventListener("click", () => {
-  if (currentIntersect) {
-    switch (currentIntersect.object) {
-      case object1:
-        console.log("click on object 1");
-        break;
-      case object2:
-        console.log("click on object 2");
-        break;
-      case object3:
-        console.log("click on object 3");
-        break;
-    }
-  }
-});
-
-/**
  * Camera
  */
 // Base camera
@@ -130,11 +96,12 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.z = 3;
+camera.position.set(-8, 4, 8);
 scene.add(camera);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
+controls.target.set(0, 1, 0);
 controls.enableDamping = true;
 
 /**
@@ -143,6 +110,8 @@ controls.enableDamping = true;
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
@@ -150,52 +119,15 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  * Animate
  */
 const clock = new THREE.Clock();
-
-let currentIntersect = null;
+let previousTime = 0;
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
 
-  // Animate objects
-  object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
-  object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
-  object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
-
-  // Cast a ray
-  raycaster.setFromCamera(mouse, camera);
-
-  // Intersections
-  const objectsToTest = [object1, object2, object3];
-  const intersects = raycaster.intersectObjects(objectsToTest);
-
-  for (const object of objectsToTest) {
-    object.material.color.set("#ff0000");
-  }
-
-  for (const intersect of intersects) {
-    intersect.object.material.color.set("#00FF00");
-  }
-
-  if (intersects.length) {
-    if (currentIntersect === null) {
-      console.log("mouse enter");
-    }
-    currentIntersect = intersects[0];
-  } else {
-    if (currentIntersect) {
-      console.log("mouse leave");
-    }
-    currentIntersect = null;
-  }
-
-  // Duck test
-  if (model) {
-    const modelIntersects = raycaster.intersectObject(model);
-    if (modelIntersects.length) {
-      model.scale.set(1.2, 1.2, 1.2);
-    } else {
-      model.scale.set(1, 1, 1);
-    }
+  if (mixer) {
+    mixer.update(deltaTime);
   }
 
   // Update controls
