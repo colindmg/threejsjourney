a066497555a36c1a78693dd3f4a829c9fb5ebd5d
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-08-21 23:00:37 +0200

    Tone mapping et anti-aliasing

--- main.js
+++ main.js
@@ -1,23 +1,13 @@
 import GUI from "lil-gui";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
-import "./style.css";
 
 /**
  * Loaders
  */
-const dracoLoader = new DRACOLoader();
-dracoLoader.setDecoderPath("/draco/");
-
 const gltfLoader = new GLTFLoader();
-gltfLoader.setDRACOLoader(dracoLoader);
-
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-const textureLoader = new THREE.TextureLoader();
-
 const rgbeLoader = new RGBELoader();
 
 /**
@@ -33,144 +23,40 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 /**
+ * Update all materials
+ */
+const updateAllMaterials = () => {
+  scene.traverse((child) => {
+    if (child.isMesh) {
+      // Activate shadow here
+    }
+  });
+};
+
+/**
  * Environment map
  */
+// Intensity
+scene.environmentIntensity = 1;
+gui.add(scene, "environmentIntensity").min(0).max(10).step(0.001);
 
-scene.environmentIntensity = 1; // Permet de régler l'intensité de l'environnement
-scene.backgroundBlurriness = 0; // Permet de régler le flou de l'arrière plan
-scene.backgroundIntensity = 1; // Permet de régler l'intensité de l'arrière plan
-// scene.backgroundRotation.x = 1; // Permet de régler la rotation de l'arrière plan
-// scene.environmentRotation.x = 1; // Permet de régler la rotation de l'environnement
+// HDR (RGBE) equirectangular
+rgbeLoader.load("/environmentMaps/0/2k.hdr", (environmentMap) => {
+  environmentMap.mapping = THREE.EquirectangularReflectionMapping;
 
-gui
-  .add(scene, "environmentIntensity")
-  .min(0)
-  .max(10)
-  .step(0.01)
-  .name("Environment Intensity");
-
-gui
-  .add(scene, "backgroundBlurriness")
-  .min(0)
-  .max(1)
-  .step(0.01)
-  .name("Background Blurriness");
-
-gui
-  .add(scene, "backgroundIntensity")
-  .min(0)
-  .max(10)
-  .step(0.01)
-  .name("Background Intensity");
-
-gui
-  .add(scene.backgroundRotation, "y")
-  .min(0)
-  .max(Math.PI * 2)
-  .step(0.01)
-  .name("Background Rotation Y");
-
-gui
-  .add(scene.environmentRotation, "y")
-  .min(0)
-  .max(Math.PI * 2)
-  .step(0.01)
-  .name("Environment Rotation Y");
-
-// LDR cube texture
-// const environmentMapLDR = cubeTextureLoader.load([
-//   "/environmentMaps/0/px.png",
-//   "/environmentMaps/0/nx.png",
-//   "/environmentMaps/0/py.png",
-//   "/environmentMaps/0/ny.png",
-//   "/environmentMaps/0/pz.png",
-//   "/environmentMaps/0/nz.png",
-// ]);
-
-// scene.environment = environmentMapLDR;
-// scene.background = environmentMapLDR;
-
-// // HDR (RGBE) equi-rectangular
-// rgbeLoader.load("/environmentMaps/blender2-2k.hdr", (texture) => {
-//   texture.mapping = THREE.EquirectangularReflectionMapping;
-//   scene.environment = texture;
-//   // scene.background = texture;
-// });
-
-// // LDR equi-rectangular
-// const environmentMapLDREqui = textureLoader.load(
-//   "/environmentMaps/blockadesLabsSkybox/fantasy_lands_castles_at_night.jpg"
-// );
-// environmentMapLDREqui.mapping = THREE.EquirectangularReflectionMapping;
-// environmentMapLDREqui.colorSpace = THREE.SRGBColorSpace;
-// scene.environment = environmentMapLDREqui;
-// scene.background = environmentMapLDREqui;
-
-// // Ground projected skybox
-// rgbeLoader.load("/environmentMaps/2/2k.hdr", (environmentMap) => {
-//   environmentMap.mapping = THREE.EquirectangularReflectionMapping;
-//   scene.environment = environmentMap;
-//   // scene.background = texture;
-
-//   // Skybox
-//   const skybox = new GroundedSkybox(environmentMap, 15, 70, 32);
-//   skybox.position.y = 15;
-//   scene.add(skybox);
-// });
-
-/**
- * Real time environment map
- */
-const environmentMapLDREqui = textureLoader.load(
-  "/environmentMaps/blockadesLabsSkybox/interior_views_cozy_wood_cabin_with_cauldron_and_p.jpg"
-);
-environmentMapLDREqui.mapping = THREE.EquirectangularReflectionMapping;
-environmentMapLDREqui.colorSpace = THREE.SRGBColorSpace;
-scene.background = environmentMapLDREqui;
-
-// Holy donut
-const holyDonut = new THREE.Mesh(
-  new THREE.TorusGeometry(8, 0.5),
-  new THREE.MeshBasicMaterial({
-    color: new THREE.Color(10, 4, 2),
-  })
-);
-holyDonut.position.y = 3.5;
-holyDonut.layers.enable(1);
-scene.add(holyDonut);
-
-// Cube render target
-const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(256, {
-  type: THREE.HalfFloatType,
+  scene.background = environmentMap;
+  scene.environment = environmentMap;
 });
-scene.environment = cubeRenderTarget.texture;
-
-// Cube camera
-const cubeCamera = new THREE.CubeCamera(0.1, 100, cubeRenderTarget);
-cubeCamera.layers.set(1);
-
-/**
- * Torus Knot
- */
-const torusKnot = new THREE.Mesh(
-  new THREE.TorusKnotGeometry(1, 0.4, 100, 16),
-  new THREE.MeshStandardMaterial({
-    roughness: 0.3,
-    metalness: 1,
-    color: 0xaaaaaa,
-  })
-);
-// torusKnot.material.envMap = environmentMapLDR;
-torusKnot.position.x = -4;
-torusKnot.position.y = 4;
-scene.add(torusKnot);
 
 /**
  * Models
  */
+// Helmet
 gltfLoader.load("/models/FlightHelmet/glTF/FlightHelmet.gltf", (gltf) => {
   gltf.scene.scale.set(10, 10, 10);
   scene.add(gltf.scene);
+
+  updateAllMaterials();
 });
 
 /**
@@ -218,25 +104,29 @@ controls.enableDamping = true;
  */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
+  antialias: true,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+// Tone mapping
+renderer.toneMapping = THREE.ReinhardToneMapping;
+renderer.toneMappingExposure = 3;
+
+gui.add(renderer, "toneMapping", {
+  No: THREE.NoToneMapping,
+  Linear: THREE.LinearToneMapping,
+  Reinhard: THREE.ReinhardToneMapping,
+  Cineon: THREE.CineonToneMapping,
+  ACESFilmic: THREE.ACESFilmicToneMapping,
+});
+
+gui.add(renderer, "toneMappingExposure").min(0).max(10).step(0.1);
+
 /**
  * Animate
  */
-const clock = new THREE.Clock();
 const tick = () => {
-  // Time
-  const elapsedTime = clock.getElapsedTime();
-
-  // Real time environment map
-  if (holyDonut) {
-    holyDonut.rotation.x = Math.sin(elapsedTime) * 2;
-
-    cubeCamera.update(renderer, scene);
-  }
-
   // Update controls
   controls.update();
 
