d366171be9ca39e22cf5df8371a5c490ec780e6f
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-08-24 15:35:08 +0200

    Utilisation d'un shaderMaterial au lieu d'un raw pour tester les variables provided de base

--- src/main.js
+++ src/main.js
@@ -41,7 +41,7 @@ for (let i = 0; i < count; i++) {
 geometry.setAttribute("aRandom", new THREE.BufferAttribute(randoms, 1));
 
 // Material
-const material = new THREE.RawShaderMaterial({
+const material = new THREE.ShaderMaterial({
   vertexShader: testVertexShader,
   fragmentShader: testFragmentShader,
   // wireframe: true,
