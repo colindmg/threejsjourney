e04c9ec85d5569053ca1e3d75dd325450765ceb9
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-08-24 19:32:40 +0200

    25 premiers patterns de la leçon 28

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import GUI from "lil-gui";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import "./style.css";
-
 import testFragmentShader from "./shaders/test/fragment.glsl";
 import testVertexShader from "./shaders/test/vertex.glsl";
+import "./style.css";
 
 /**
  * Base
@@ -17,60 +16,23 @@ const canvas = document.querySelector("canvas.webgl");
 
 // Scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x141414);
-
-/**
- * Textures
- */
-const textureLoader = new THREE.TextureLoader();
-const flagTexture = textureLoader.load("/textures/perso-flag.png");
 
 /**
  * Test mesh
  */
 // Geometry
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
-
-const count = geometry.attributes.position.count; // Extract the number of vertices in the geometry
-const randoms = new Float32Array(count);
-
-for (let i = 0; i < count; i++) {
-  randoms[i] = Math.random();
-}
-
-geometry.setAttribute("aRandom", new THREE.BufferAttribute(randoms, 1));
+// const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5, 32, 32, 32);
 
 // Material
 const material = new THREE.ShaderMaterial({
   vertexShader: testVertexShader,
   fragmentShader: testFragmentShader,
-  // wireframe: true,
-  transparent: true,
-  uniforms: {
-    uFrequency: { value: new THREE.Vector2(10, 5) },
-    uTime: { value: 0 },
-    uColor: { value: new THREE.Color("orange") },
-    uTexture: { value: flagTexture },
-  },
+  side: THREE.DoubleSide,
 });
-
-gui
-  .add(material.uniforms.uFrequency.value, "x")
-  .min(0)
-  .max(20)
-  .step(0.01)
-  .name("frequencyX");
-
-gui
-  .add(material.uniforms.uFrequency.value, "y")
-  .min(0)
-  .max(20)
-  .step(0.01)
-  .name("frequencyY");
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material);
-// mesh.scale.y = 2 / 3;
 scene.add(mesh);
 
 /**
@@ -124,14 +86,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 /**
  * Animate
  */
-const clock = new THREE.Clock();
-
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-
-  // Update material
-  material.uniforms.uTime.value = elapsedTime;
-
   // Update controls
   controls.update();
 
