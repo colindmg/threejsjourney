d08b4d916784c97ba7f371f4c2e229d6f38bf6ba
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-08-24 13:54:55 +0200

    Séparation des shaders dans des fichiers différents

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import "./style.css";
 
+import testFragmentShader from "./shaders/test/fragment.glsl";
+import testVertexShader from "./shaders/test/vertex.glsl";
+
 /**
  * Base
  */
@@ -28,24 +31,9 @@ const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
 
 // Material
 const material = new THREE.RawShaderMaterial({
-  vertexShader: `
-    uniform mat4 projectionMatrix;
-    uniform mat4 viewMatrix;
-    uniform mat4 modelMatrix;
-
-    attribute vec3 position;
-
-    void main() {
-      gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
-    }
-  `,
-  fragmentShader: `
-    precision mediump float;
-
-    void main() {
-      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    }
-  `,
+  vertexShader: testVertexShader,
+  fragmentShader: testFragmentShader,
+  // wireframe: true,
 });
 
 // Mesh
