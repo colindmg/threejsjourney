707ad0729599858f9bbf49b3d984d6ef10c7d5f3
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-08-26 23:42:13 +0200

    Anim fun d'un des pattern

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import GUI from "lil-gui";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import testFragmentShader from "./shaders/test/fragment.glsl";
@@ -9,34 +8,68 @@ import "./style.css";
  * Base
  */
 // Debug
-const gui = new GUI();
+// const gui = new GUI();
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
 // Scene
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x141414);
 
 /**
  * Test mesh
  */
 // Geometry
-const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
-// const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5, 32, 32, 32);
+// const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
+const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5, 32, 32, 32);
+const sphereGeometry = new THREE.SphereGeometry(0.25, 32, 32);
 
 // Material
 const material = new THREE.ShaderMaterial({
   vertexShader: testVertexShader,
   fragmentShader: testFragmentShader,
-  side: THREE.DoubleSide,
+  // side: THREE.DoubleSide,
   uniforms: {
     uTime: { value: 0 },
   },
 });
 
 // Mesh
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const mesh1 = new THREE.Mesh(geometry, material);
+mesh1.position.x = -0.75;
+
+const mesh2 = new THREE.Mesh(sphereGeometry, material);
+mesh2.position.x = 0;
+
+const mesh3 = new THREE.Mesh(geometry, material);
+mesh3.position.x = 0.75;
+
+const mesh4 = new THREE.Mesh(sphereGeometry, material);
+mesh4.position.x = 0.75;
+mesh4.position.y = 0.75;
+
+const mesh5 = new THREE.Mesh(geometry, material);
+mesh5.position.x = 0;
+mesh5.position.y = 0.75;
+
+const mesh6 = new THREE.Mesh(sphereGeometry, material);
+mesh6.position.x = -0.75;
+mesh6.position.y = -0.75;
+
+const mesh7 = new THREE.Mesh(geometry, material);
+mesh7.position.x = 0;
+mesh7.position.y = -0.75;
+
+const mesh8 = new THREE.Mesh(sphereGeometry, material);
+mesh8.position.x = 0.75;
+mesh8.position.y = -0.75;
+
+const mesh9 = new THREE.Mesh(sphereGeometry, material);
+mesh9.position.x = -0.75;
+mesh9.position.y = 0.75;
+
+scene.add(mesh1, mesh2, mesh3, mesh4, mesh5, mesh6, mesh7, mesh8, mesh9);
 
 /**
  * Sizes
@@ -70,7 +103,8 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.set(0.25, -0.25, 1);
+// camera.position.set(0.25, -0.25, 1);
+camera.position.set(0, 0, 2);
 scene.add(camera);
 
 // Controls
--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -239,11 +239,11 @@ void main()
 	// float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
 
 	// Pattern 39
-	// vec2 wavedUv = vec2(
-	// 	vUv.x + sin(vUv.y * 100.0) * 0.1,
-	// 	vUv.y + sin(vUv.x * 100.0) * 0.1
-	// );
-	// float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
+	vec2 wavedUv = vec2(
+		vUv.x + sin(vUv.y * 10.0 * uTime ) * 0.1,
+		vUv.y + sin(vUv.x * 10.0 * uTime ) * 0.1
+	);
+	float strength = step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25)) - 0.1;
 
 	// Pattern 40
 	// float angle = atan(vUv.x, vUv.y);
@@ -295,7 +295,7 @@ void main()
 	// float strength = sin(cnoise(vUv * 10.0) * 20.0);
 
 	// Pattern 50
-	float strength = step(0.9, sin(cnoise(vUv * 10.0) * 20.0));
+	// float strength = step(0.9, sin(cnoise(vUv * 10.0) * 20.0));
 
 	// ----------------------------
 
@@ -303,12 +303,12 @@ void main()
 	strength = clamp(strength, 0.0, 1.0);
 
 	// Colored version
-	vec3 blackColor = vec3(0.0);
-	vec3 uvColor = vec3(1.0, vUv);
-	vec3 mixedColor = mix(blackColor, uvColor, strength);
+	// vec3 blackColor = vec3(0.0);
+	// vec3 uvColor = vec3(0.6, vUv);
+	// vec3 mixedColor = mix(blackColor, uvColor, strength);
 
-	gl_FragColor = vec4(mixedColor, 1.0);
+	// gl_FragColor = vec4(mixedColor, 1.0);
 
 	// Black and white version
-  // gl_FragColor = vec4(vec3(strength), 1.0);
+  gl_FragColor = vec4(vec3(strength), 1.0);
 }
