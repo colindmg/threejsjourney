9639af5692efaf79f14fbef471a8297227eaffc0
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-08-31 14:26:44 +0200

    Plane et shaders de base pour la coffeesmoke

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import GUI from "lil-gui";
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import coffeeSmokeFragmentShader from "./shaders/coffeeSmoke/fragment.glsl";
+import coffeeSmokeVertexShader from "./shaders/coffeeSmoke/vertex.glsl";
 
 /**
  * Base
@@ -15,171 +17,9 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
-/**
- * Loaders
- */
+// Loaders
 const textureLoader = new THREE.TextureLoader();
 const gltfLoader = new GLTFLoader();
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-
-/**
- * Update all materials
- */
-const updateAllMaterials = () => {
-  scene.traverse((child) => {
-    if (
-      child instanceof THREE.Mesh &&
-      child.material instanceof THREE.MeshStandardMaterial
-    ) {
-      child.material.envMapIntensity = 1;
-      child.material.needsUpdate = true;
-      child.castShadow = true;
-      child.receiveShadow = true;
-    }
-  });
-};
-
-/**
- * Environment map
- */
-const environmentMap = cubeTextureLoader.load([
-  "/textures/environmentMaps/0/px.jpg",
-  "/textures/environmentMaps/0/nx.jpg",
-  "/textures/environmentMaps/0/py.jpg",
-  "/textures/environmentMaps/0/ny.jpg",
-  "/textures/environmentMaps/0/pz.jpg",
-  "/textures/environmentMaps/0/nz.jpg",
-]);
-
-scene.background = environmentMap;
-scene.environment = environmentMap;
-
-/**
- * Material
- */
-
-// Textures
-const mapTexture = textureLoader.load("/models/LeePerrySmith/color.jpg");
-mapTexture.colorSpace = THREE.SRGBColorSpace;
-const normalTexture = textureLoader.load("/models/LeePerrySmith/normal.jpg");
-
-// Material
-const material = new THREE.MeshStandardMaterial({
-  map: mapTexture,
-  normalMap: normalTexture,
-});
-
-const depthMaterial = new THREE.MeshDepthMaterial({
-  depthPacking: THREE.RGBADepthPacking,
-});
-
-const customUniforms = {
-  uTime: { value: 0 },
-};
-
-material.onBeforeCompile = (shader) => {
-  shader.uniforms.uTime = customUniforms.uTime;
-
-  shader.vertexShader = shader.vertexShader.replace(
-    "#include <common>",
-    `
-		#include <common>
-
-		uniform float uTime;
-
-		mat2 get2dRotateMatrix(float _angle) {
-			return mat2(cos(_angle), -sin(_angle), sin(_angle), cos(_angle));
-		}
-		`
-  );
-
-  shader.vertexShader = shader.vertexShader.replace(
-    "#include <beginnormal_vertex>",
-    `
-		#include <beginnormal_vertex>
-
-		float angle = (position.y + uTime) * 0.9;
-		mat2 rotateMatrix = get2dRotateMatrix(angle);
-
-		objectNormal.xz = rotateMatrix * objectNormal.xz;
-		`
-  );
-
-  shader.vertexShader = shader.vertexShader.replace(
-    "#include <begin_vertex>",
-    `
-		#include <begin_vertex>
-		transformed.xz = rotateMatrix * transformed.xz;
-		`
-  );
-};
-
-depthMaterial.onBeforeCompile = (shader) => {
-  shader.uniforms.uTime = customUniforms.uTime;
-
-  shader.vertexShader = shader.vertexShader.replace(
-    "#include <common>",
-    `
-		#include <common>
-
-		uniform float uTime;
-
-		mat2 get2dRotateMatrix(float _angle) {
-			return mat2(cos(_angle), -sin(_angle), sin(_angle), cos(_angle));
-		}
-		`
-  );
-
-  shader.vertexShader = shader.vertexShader.replace(
-    "#include <begin_vertex>",
-    `
-		#include <begin_vertex>
-
-		float angle = (position.y + uTime) * 0.9;
-		mat2 rotateMatrix = get2dRotateMatrix(angle);
-
-		transformed.xz = rotateMatrix * transformed.xz;
-		`
-  );
-};
-
-/**
- * Models
- */
-gltfLoader.load("/models/LeePerrySmith/LeePerrySmith.glb", (gltf) => {
-  // Model
-  const mesh = gltf.scene.children[0];
-  mesh.rotation.y = Math.PI * 0.5;
-  mesh.material = material;
-  mesh.customDepthMaterial = depthMaterial;
-  scene.add(mesh);
-
-  // Update materials
-  updateAllMaterials();
-});
-
-/**
- * Plane
- */
-const plane = new THREE.Mesh(
-  new THREE.PlaneGeometry(15, 15, 15),
-  new THREE.MeshStandardMaterial()
-);
-plane.rotation.y = Math.PI;
-plane.position.y = -5;
-plane.position.z = 5;
-scene.add(plane);
-
-/**
- * Lights
- */
-const directionalLight = new THREE.DirectionalLight("#ffffff", 3);
-directionalLight.castShadow = true;
-directionalLight.shadow.mapSize.set(1024, 1024);
-directionalLight.shadow.camera.far = 15;
-directionalLight.shadow.normalBias = 0.05;
-directionalLight.position.set(0.25, 2, -2.25);
-scene.add(directionalLight);
 
 /**
  * Sizes
@@ -208,16 +48,19 @@ window.addEventListener("resize", () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(
-  75,
+  25,
   sizes.width / sizes.height,
   0.1,
   100
 );
-camera.position.set(4, 1, -4);
+camera.position.x = 8;
+camera.position.y = 10;
+camera.position.z = 12;
 scene.add(camera);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
+controls.target.y = 3;
 controls.enableDamping = true;
 
 /**
@@ -227,12 +70,38 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
   antialias: true,
 });
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFShadowMap;
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 1;
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Model
+ */
+gltfLoader.load("./bakedModel.glb", (gltf) => {
+  gltf.scene.getObjectByName("baked").material.map.anisotropy = 8;
+  scene.add(gltf.scene);
+});
+
+/**
+ * Smoke
+ */
+
+// Geometry
+const smokeGeometry = new THREE.PlaneGeometry(1, 1, 16, 64);
+smokeGeometry.translate(0, 0.5, 0);
+smokeGeometry.scale(1.5, 6, 1.5);
+
+// Material
+const smokeMaterial = new THREE.ShaderMaterial({
+  // wireframe: true,
+  side: THREE.DoubleSide,
+  vertexShader: coffeeSmokeVertexShader,
+  fragmentShader: coffeeSmokeFragmentShader,
+});
+
+// Mesh
+const smoke = new THREE.Mesh(smokeGeometry, smokeMaterial);
+smoke.position.y = 1.83;
+scene.add(smoke);
 
 /**
  * Animate
@@ -241,9 +110,6 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-
-  // Update custom uniforms
-  customUniforms.uTime.value = elapsedTime;
 
   // Update controls
   controls.update();
