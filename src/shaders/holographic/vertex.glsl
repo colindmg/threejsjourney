9444e6c15aab4da4d053c0fe6d128a4f2fe29cf4
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-09-01 15:40:16 +0200

    Utilisation de fresnel et fix des normal

--- src/shaders/holographic/vertex.glsl
+++ src/shaders/holographic/vertex.glsl
@@ -1,4 +1,5 @@
 varying vec3 vPosition;
+varying vec3 vNormal;
 
 void main()
 {
@@ -8,6 +9,10 @@ void main()
   // Final position
   gl_Position = projectionMatrix * viewMatrix * modelPosition;
 
+  // Model normal
+  vec4 modelNormal = modelMatrix * vec4(normal, 0.0);
+
   // Varying
   vPosition = modelPosition.xyz;
+  vNormal = modelNormal.xyz;
 }
