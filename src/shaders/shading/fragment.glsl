99cd082f22c007cb69dbc42c5cfcac9ddcdb7e10
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-09-04 21:53:40 +0200

    Créer une ambient light avec le fragment shader

--- src/shaders/shading/fragment.glsl
+++ src/shaders/shading/fragment.glsl
@@ -1,8 +1,19 @@
 uniform vec3 uColor;
 
+#include ../includes/ambientLight.glsl
+
 void main()
 {
 	vec3 color = uColor;
+
+	// Lights
+	vec3 light = vec3(0.0);
+	light += ambientLight(
+		vec3(1.0, 0.0, 0.0), // Light color
+		0.2                  // Light intensity
+	);
+
+	color *= light;
 
 	// Final color
 	gl_FragColor = vec4(color, 1.0);
