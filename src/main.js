7c58cafddee430480a96c25f32419421c576c350
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-09-04 10:42:58 +0200

    Starter de la leçon 35 - Light shading

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
-import gsap from "gsap";
 import GUI from "lil-gui";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { Sky } from "three/addons/objects/Sky.js";
-import fireworkFragmentShader from "./shaders/firework/fragment.glsl";
-import fireworkVertexShader from "./shaders/firework/vertex.glsl";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import shadingFragmentShader from "./shaders/shading/fragment.glsl";
+import shadingVertexShader from "./shaders/shading/vertex.glsl";
 
 /**
  * Base
  */
 // Debug
-const gui = new GUI({ width: 340 });
+const gui = new GUI();
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -19,7 +18,7 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 // Loaders
-const textureLoader = new THREE.TextureLoader();
+const gltfLoader = new GLTFLoader();
 
 /**
  * Sizes
@@ -29,23 +28,11 @@ const sizes = {
   height: window.innerHeight,
   pixelRatio: Math.min(window.devicePixelRatio, 2),
 };
-sizes.resolution = new THREE.Vector2(
-  sizes.width * sizes.pixelRatio,
-  sizes.height * sizes.pixelRatio
-);
 
 window.addEventListener("resize", () => {
   // Update sizes
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
-
-  // Update resolution
-  sizes.resolution.set(
-    sizes.width * sizes.pixelRatio,
-    sizes.height * sizes.pixelRatio
-  );
-
-  // Update pixel ratio
   sizes.pixelRatio = Math.min(window.devicePixelRatio, 2);
 
   // Update camera
@@ -67,7 +54,9 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.set(1.5, 0, 6);
+camera.position.x = 7;
+camera.position.y = 7;
+camera.position.z = 7;
 scene.add(camera);
 
 // Controls
@@ -81,176 +70,75 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
   antialias: true,
 });
+// renderer.toneMapping = THREE.ACESFilmicToneMapping
+// renderer.toneMappingExposure = 3
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(sizes.pixelRatio);
 
 /**
- * Textures
+ * Material
  */
-const textures = [
-  textureLoader.load("/particles/1.png"),
-  textureLoader.load("/particles/2.png"),
-  textureLoader.load("/particles/3.png"),
-  textureLoader.load("/particles/4.png"),
-  textureLoader.load("/particles/5.png"),
-  textureLoader.load("/particles/6.png"),
-  textureLoader.load("/particles/7.png"),
-  textureLoader.load("/particles/8.png"),
-];
+const materialParameters = {};
+materialParameters.color = "#ffffff";
+
+const material = new THREE.ShaderMaterial({
+  vertexShader: shadingVertexShader,
+  fragmentShader: shadingFragmentShader,
+  uniforms: {
+    uColor: new THREE.Uniform(new THREE.Color(materialParameters.color)),
+  },
+});
+
+gui.addColor(materialParameters, "color").onChange(() => {
+  material.uniforms.uColor.value.set(materialParameters.color);
+});
 
 /**
- * Fireworks
+ * Objects
  */
-const createFirework = (count, position, size, texture, radius, color) => {
-  // Geometry
-  const positionsArray = new Float32Array(count * 3);
-  const sizesArray = new Float32Array(count);
-  const timeMultipliersArray = new Float32Array(count);
+// Torus knot
+const torusKnot = new THREE.Mesh(
+  new THREE.TorusKnotGeometry(0.6, 0.25, 128, 32),
+  material
+);
+torusKnot.position.x = 3;
+scene.add(torusKnot);
 
-  for (let i = 0; i < count * 3; i++) {
-    const i3 = i * 3;
+// Sphere
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(), material);
+sphere.position.x = -3;
+scene.add(sphere);
 
-    const spherical = new THREE.Spherical(
-      radius * (0.75 + Math.random() * 0.25),
-      Math.random() * Math.PI,
-      Math.random() * Math.PI * 2
-    );
-    const position = new THREE.Vector3();
-    position.setFromSpherical(spherical);
-
-    // Position
-    positionsArray[i3 + 0] = position.x;
-    positionsArray[i3 + 1] = position.y;
-    positionsArray[i3 + 2] = position.z;
-
-    // Size
-    sizesArray[i] = Math.random();
-
-    // Time multiplier
-    timeMultipliersArray[i] = 1 + Math.random();
-  }
-
-  const geometry = new THREE.BufferGeometry();
-  geometry.setAttribute(
-    "position",
-    new THREE.BufferAttribute(positionsArray, 3)
-  );
-  geometry.setAttribute("aSize", new THREE.BufferAttribute(sizesArray, 1));
-  geometry.setAttribute(
-    "aTimeMultiplier",
-    new THREE.BufferAttribute(timeMultipliersArray, 1)
-  );
-
-  // Material
-  texture.flipY = false;
-  const material = new THREE.ShaderMaterial({
-    vertexShader: fireworkVertexShader,
-    fragmentShader: fireworkFragmentShader,
-    uniforms: {
-      uSize: new THREE.Uniform(size),
-      uResolution: new THREE.Uniform(sizes.resolution),
-      uTexture: new THREE.Uniform(texture),
-      uColor: new THREE.Uniform(color),
-      uProgress: new THREE.Uniform(0),
-    },
-    transparent: true,
-    depthWrite: false,
-    blending: THREE.AdditiveBlending,
+// Suzanne
+let suzanne = null;
+gltfLoader.load("./suzanne.glb", (gltf) => {
+  suzanne = gltf.scene;
+  suzanne.traverse((child) => {
+    if (child.isMesh) child.material = material;
   });
-
-  // Points
-  const firework = new THREE.Points(geometry, material);
-  firework.position.copy(position);
-  scene.add(firework);
-
-  // Destroy
-  const destroy = () => {
-    scene.remove(firework);
-    geometry.dispose();
-    material.dispose();
-  };
-
-  // Animation
-  gsap.to(material.uniforms.uProgress, {
-    value: 1,
-    duration: 3,
-    ease: "linear",
-    onComplete: destroy,
-  });
-};
-
-// RANDOM FIREWORKS
-const createRandomFirework = () => {
-  const count = Math.round(400 + Math.random() * 1000);
-  const position = new THREE.Vector3(
-    (Math.random() - 0.5) * 2,
-    Math.random(),
-    (Math.random() - 0.5) * 2
-  );
-  const size = 0.1 + Math.random() * 0.1;
-  const texture = textures[Math.floor(Math.random() * textures.length)];
-  const radius = 0.5 + Math.random();
-  const color = new THREE.Color();
-  color.setHSL(Math.random(), 1, 0.7);
-
-  createFirework(count, position, size, texture, radius, color);
-};
-
-window.addEventListener("click", createRandomFirework);
-
-/**
- * Sky
- */
-const sky = new Sky();
-sky.scale.setScalar(450000);
-scene.add(sky);
-
-const sun = new THREE.Vector3();
-
-const effectController = {
-  turbidity: 10,
-  rayleigh: 3,
-  mieCoefficient: 0.005,
-  mieDirectionalG: 0.7,
-  elevation: 2,
-  azimuth: 180,
-  exposure: renderer.toneMappingExposure,
-};
-
-const updateSky = () => {
-  const uniforms = sky.material.uniforms;
-  uniforms["turbidity"].value = effectController.turbidity;
-  uniforms["rayleigh"].value = effectController.rayleigh;
-  uniforms["mieCoefficient"].value = effectController.mieCoefficient;
-  uniforms["mieDirectionalG"].value = effectController.mieDirectionalG;
-
-  const phi = THREE.MathUtils.degToRad(90 - effectController.elevation);
-  const theta = THREE.MathUtils.degToRad(effectController.azimuth);
-
-  sun.setFromSphericalCoords(1, phi, theta);
-
-  uniforms["sunPosition"].value.copy(sun);
-
-  renderer.toneMappingExposure = effectController.exposure;
-  renderer.render(scene, camera);
-};
-
-gui.add(effectController, "turbidity", 0.0, 20.0, 0.1).onChange(updateSky);
-gui.add(effectController, "rayleigh", 0.0, 4, 0.001).onChange(updateSky);
-gui
-  .add(effectController, "mieCoefficient", 0.0, 0.1, 0.001)
-  .onChange(updateSky);
-gui.add(effectController, "mieDirectionalG", 0.0, 1, 0.001).onChange(updateSky);
-gui.add(effectController, "elevation", -3, 10, 0.01).onChange(updateSky);
-gui.add(effectController, "azimuth", -180, 180, 0.1).onChange(updateSky);
-gui.add(effectController, "exposure", 0, 1, 0.0001).onChange(updateSky);
-
-updateSky();
+  scene.add(suzanne);
+});
 
 /**
  * Animate
  */
+const clock = new THREE.Clock();
+
 const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Rotate objects
+  if (suzanne) {
+    suzanne.rotation.x = -elapsedTime * 0.1;
+    suzanne.rotation.y = elapsedTime * 0.2;
+  }
+
+  sphere.rotation.x = -elapsedTime * 0.1;
+  sphere.rotation.y = elapsedTime * 0.2;
+
+  torusKnot.rotation.x = -elapsedTime * 0.1;
+  torusKnot.rotation.y = elapsedTime * 0.2;
+
   // Update controls
   controls.update();
 
