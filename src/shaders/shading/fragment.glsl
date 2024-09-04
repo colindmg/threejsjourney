7bfd459941c25f1a5a78ae91a906c16fe5676b08
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-09-04 22:25:40 +0200

    Matériel étrange trouvé au hasard

--- src/shaders/shading/fragment.glsl
+++ src/shaders/shading/fragment.glsl
@@ -1,39 +1,46 @@
 uniform vec3 uColor;
 
 varying vec3 vNormal;
+varying vec3 vPosition;
 
 #include ../includes/ambientLight.glsl
 
-vec3 directionalLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 lightPosition)
+vec3 directionalLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 lightPosition, vec3 viewDirection)
 {
 	vec3 lightDirection = normalize(lightPosition);
+	vec3 lightReflection = reflect(- lightDirection, normal);
 
 	// Shading
 	float shading = dot(normal, lightDirection);
+	shading = max(0.0, shading);
 
-	// return lightColor * lightIntensity;
-	return vec3(shading);
+	// Specular
+	float specular = - dot(lightReflection, viewDirection);
+
+	return lightColor * lightIntensity * shading + pow(specular, 32.0);
 }
 
 void main()
 {
+	vec3 viewDirection = normalize(vPosition - cameraPosition);
 	vec3 color = uColor;
 
 	// Lights
 	vec3 light = vec3(0.0);
 
 	// AMBIENT LIGHT
-	// light += ambientLight(
-	// 	vec3(1.0), // Light color
-	// 	0.03       // Light intensity
-	// );
+	light += ambientLight(
+		vec3(1.0), // Light color
+		0.03       // Light intensity
+	);
 
 	// DIRECTIONAL LIGHT
 	light += directionalLight(
 		vec3(0.1, 0.1, 1.0), // Light color
 		1.0,                 // Light intensity
 		vNormal,             // Normal
-		vec3(0.0, 0.0, 3.0)  // Light position
+		vec3(0.0, 0.0, 3.0), // Light position
+		viewDirection        // View direction
 	);
 
 	color *= light;
--- src/shaders/shading/vertex.glsl
+++ src/shaders/shading/vertex.glsl
@@ -1,4 +1,5 @@
 varying vec3 vNormal;
+varying vec3 vPosition;
 
 void main()
 {
@@ -6,6 +7,10 @@ void main()
 	vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 	gl_Position = projectionMatrix * viewMatrix * modelPosition;
 
+	// Model normal
+	vec4 modelNormal = modelMatrix * vec4(normal, 0.0);
+
 	// Varying
-	vNormal = normal;
+	vNormal = modelNormal.xyz;
+	vPosition = modelPosition.xyz;
 }
