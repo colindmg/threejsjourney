a711d2b752e036e845c9f7bff159f8995f78beea
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-09-05 16:24:07 +0200

    Ajout d'une directional light

--- src/shaders/water/fragment.glsl
+++ src/shaders/water/fragment.glsl
@@ -4,13 +4,34 @@ uniform float uColorOffset;
 uniform float uColorMultiplier;
 
 varying float vElevation;
+varying vec3 vNormal;
+varying vec3 vPosition;
+
+#include ../includes/directionalLight.glsl
 
 void main()
 {
+	vec3 viewDirection = normalize(vPosition - cameraPosition);
+	vec3 normal = normalize(vNormal);
+
 	// Base color
 	float mixStrength = (vElevation + uColorOffset) * uColorMultiplier;
 	mixStrength = smoothstep(0.0, 1.0, mixStrength);
 	vec3 color = mix(uDepthColor, uSurfaceColor, mixStrength);
+
+	// Lights
+	vec3 light = vec3(0.0);
+
+	light += directionalLight(
+		vec3(1.0),
+		1.0,
+		normal,
+		vec3(-1.0, 0.5, 0.0),
+		viewDirection,
+		30.0
+	);
+
+	color *= light;
 	
 	// Final color
 	gl_FragColor = vec4(color, 1.0);
