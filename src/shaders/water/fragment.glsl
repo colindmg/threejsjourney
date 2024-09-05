0b7e2146122a3df0884e075976dd882a9cecfc85
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-09-05 16:12:48 +0200

    Tweak de l'ancienne raging sea, séparation de la perlin noise et ajout des 3 lights faites dans la leçon précédente

--- src/shaders/water/fragment.glsl
+++ src/shaders/water/fragment.glsl
@@ -7,9 +7,13 @@ varying float vElevation;
 
 void main()
 {
+	// Base color
 	float mixStrength = (vElevation + uColorOffset) * uColorMultiplier;
+	mixStrength = smoothstep(0.0, 1.0, mixStrength);
 	vec3 color = mix(uDepthColor, uSurfaceColor, mixStrength);
 	
+	// Final color
 	gl_FragColor = vec4(color, 1.0);
+	#include <tonemapping_fragment>
 	#include <colorspace_fragment>
 }
