ba06f9f56541a1bac4fef9df244845059b02b220
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-09-05 17:00:07 +0200

    Ajout de tone mapping et suppression du helper temporaire

--- src/main.js
+++ src/main.js
@@ -18,15 +18,17 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 // Axes helper
-const axesHelper = new THREE.AxesHelper();
-axesHelper.position.y = 0.25;
-scene.add(axesHelper);
+// const axesHelper = new THREE.AxesHelper();
+// axesHelper.position.y = 0.25;
+// scene.add(axesHelper);
 
 /**
  * Water
  */
 // Geometry
 const waterGeometry = new THREE.PlaneGeometry(2, 2, 512, 512);
+waterGeometry.deleteAttribute("normal");
+waterGeometry.deleteAttribute("uv");
 
 // Colors
 debugObject.depthColor = "#FF4000";
@@ -175,6 +177,7 @@ controls.enableDamping = true;
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
--- src/shaders/water/fragment.glsl
+++ src/shaders/water/fragment.glsl
@@ -7,7 +7,7 @@ varying float vElevation;
 varying vec3 vNormal;
 varying vec3 vPosition;
 
-#include ../includes/directionalLight.glsl
+#include ../includes/pointLight.glsl
 
 void main()
 {
@@ -22,20 +22,22 @@ void main()
 	// Lights
 	vec3 light = vec3(0.0);
 
-	light += directionalLight(
-		vec3(1.0),
-		1.0,
-		normal,
-		vec3(-1.0, 0.5, 0.0),
-		viewDirection,
-		30.0
+	light += pointLight(
+		vec3(1.0),            // Light color
+		10.0, 							  // Light intensity
+		normal, 						  // Normal
+		vec3(0.0, 0.25, 0.0), // Light position
+		viewDirection,        // View direction
+		30.0,                 // Specular power
+		vPosition,            // Position
+		0.95                  // Decay
 	);
 
 	color *= light;
 	
 	// Final color
-	// gl_FragColor = vec4(color, 1.0);
-	gl_FragColor = vec4(normal, 1.0);
+	gl_FragColor = vec4(color, 1.0);
+	// gl_FragColor = vec4(normal, 1.0);
 	#include <tonemapping_fragment>
 	#include <colorspace_fragment>
 }
