01a3e1f693fccb07f2571c8c94a959bd65be2762
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-09-11 20:43:16 +0200

    Changement du radius et de l'intensité des cercles pour faire comme si c'était des lights

--- src/shaders/halftone/fragment.glsl
+++ src/shaders/halftone/fragment.glsl
@@ -34,13 +34,18 @@ void main()
 
 	// Halftone
 	float repetitions = 50.0;
+	vec3 direction = vec3(0.0, -1.0, 0.0);
+	float low = -0.8;
+	float high = 1.5;
+	float intensity = dot(normal, direction);
+	intensity = smoothstep(low, high, intensity);
 
 	vec2 uv = gl_FragCoord.xy / uResolution.y;
 	uv *= repetitions;
 	uv = mod(uv, 1.0);
 
 	float point = distance(uv, vec2(0.5));
-	point = 1. - step(0.5 * 0.3, point);
+	point = 1. - step(0.5 * intensity, point);
 	
 
 	// Final color
