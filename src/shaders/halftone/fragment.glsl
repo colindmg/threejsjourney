4435b49e86da51ca35850983806d26b61c89e889
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-09-11 20:48:52 +0200

    Mise de l'effet de halftone dans une fonction séparée

--- src/shaders/halftone/fragment.glsl
+++ src/shaders/halftone/fragment.glsl
@@ -7,6 +7,29 @@ varying vec3 vPosition;
 #include ../includes/ambientLight.glsl
 #include ../includes/directionalLight.glsl
 
+vec3 halftone(
+	vec3 color, 
+	float repetitions,
+	vec3 direction, 
+	float low,
+	float high,
+	vec3 pointColor,
+	vec3 normal
+) 
+{
+	float intensity = dot(normal, direction);
+	intensity = smoothstep(low, high, intensity);
+
+	vec2 uv = gl_FragCoord.xy / uResolution.y;
+	uv *= repetitions;
+	uv = mod(uv, 1.0);
+
+	float point = distance(uv, vec2(0.5));
+	point = 1. - step(0.5 * intensity, point);
+
+	return mix(color, pointColor, point);
+}
+
 void main()
 {
 	vec3 viewDirection = normalize(vPosition - cameraPosition);
@@ -33,24 +56,7 @@ void main()
 	color *= light;
 
 	// Halftone
-	float repetitions = 50.0;
-	vec3 direction = vec3(0.0, -1.0, 0.0);
-	float low = -0.8;
-	float high = 1.5;
-	vec3 pointColor = vec3(1.0, 0.0, 0.0);
-
-	float intensity = dot(normal, direction);
-	intensity = smoothstep(low, high, intensity);
-
-	vec2 uv = gl_FragCoord.xy / uResolution.y;
-	uv *= repetitions;
-	uv = mod(uv, 1.0);
-
-	float point = distance(uv, vec2(0.5));
-	point = 1. - step(0.5 * intensity, point);
-
-	color = mix(color, pointColor, point);
-	
+	color = halftone(color, 50.0, vec3(0.0, -1.0, 0.0), -0.8, 1.5, vec3(1.0, 0.0, 0.0), normal);
 
 	// Final color
 	gl_FragColor = vec4(color, 1.0);
