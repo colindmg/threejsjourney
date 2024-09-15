6c812d522808efc5644ac98ee055a24a5dfe5194
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-09-15 22:41:47 +0200

    Récupération des uniforms dans le fragment shader

--- src/shaders/earth/fragment.glsl
+++ src/shaders/earth/fragment.glsl
@@ -1,3 +1,7 @@
+uniform sampler2D uDayTexture;
+uniform sampler2D uNightTexture;
+uniform float uSpecularCloudsTexture;
+
 varying vec2 vUv;
 varying vec3 vNormal;
 varying vec3 vPosition;
@@ -6,7 +10,7 @@ void main()
 {
     vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 normal = normalize(vNormal);
-    vec3 color = vec3(vUv, 1.0);
+    vec3 color = vec3(0.0);
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
