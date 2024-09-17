46decd7980ecf8d6b055213a30dff1531c899a6d
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-09-17 19:03:06 +0200

    Forme des particules et changement de leur taille/couleur en fonction de l'image qui leur est appliquée

--- src/main.js
+++ src/main.js
@@ -76,7 +76,7 @@ renderer.setPixelRatio(sizes.pixelRatio);
 /**
  * Particles
  */
-const particlesGeometry = new THREE.PlaneGeometry(10, 10, 32, 32);
+const particlesGeometry = new THREE.PlaneGeometry(10, 10, 128, 128);
 
 const particlesMaterial = new THREE.ShaderMaterial({
   vertexShader: particlesVertexShader,
@@ -88,6 +88,7 @@ const particlesMaterial = new THREE.ShaderMaterial({
         sizes.height * sizes.pixelRatio
       )
     ),
+    uPictureTexture: new THREE.Uniform(textureLoader.load("/picture-1.png")),
   },
 });
 const particles = new THREE.Points(particlesGeometry, particlesMaterial);
