14b0dcafcd63fa2fe095d58ced1dff309a6a8c53
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-09-17 18:27:43 +0200

    Starter de la leçon 39 - Particles Cursor Animation

--- src/main.js
+++ src/main.js
@@ -1,17 +1,11 @@
-import GUI from "lil-gui";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import atmosphereFragmentShader from "./shaders/atmosphere/fragment.glsl";
-import atmosphereVertexShader from "./shaders/atmosphere/vertex.glsl";
-import earthFragmentShader from "./shaders/earth/fragment.glsl";
-import earthVertexShader from "./shaders/earth/vertex.glsl";
+import particlesFragmentShader from "./shaders/particles/fragment.glsl";
+import particlesVertexShader from "./shaders/particles/vertex.glsl";
 
 /**
  * Base
  */
-// Debug
-const gui = new GUI();
-
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
@@ -20,139 +14,6 @@ const scene = new THREE.Scene();
 
 // Loaders
 const textureLoader = new THREE.TextureLoader();
-
-/**
- * Earth
- */
-// Parameters
-const earthParameters = {};
-earthParameters.atmosphereDayColor = "#00AAFF";
-earthParameters.atmosphereTwilightColor = "#FF6600";
-
-gui
-  .addColor(earthParameters, "atmosphereDayColor")
-  .name("atmosphere day")
-  .onChange(() => {
-    earthMaterial.uniforms.uAtmosphereDayColor.value.set(
-      earthParameters.atmosphereDayColor
-    );
-    atmosphereMaterial.uniforms.uAtmosphereDayColor.value.set(
-      earthParameters.atmosphereDayColor
-    );
-  });
-gui
-  .addColor(earthParameters, "atmosphereTwilightColor")
-  .name("atmosphere twilight")
-  .onChange(() => {
-    earthMaterial.uniforms.uAtmosphereTwilightColor.value.set(
-      earthParameters.atmosphereTwilightColor
-    );
-    atmosphereMaterial.uniforms.uAtmosphereTwilightColor.value.set(
-      earthParameters.atmosphereTwilightColor
-    );
-  });
-
-// Textures
-const earthDayTexture = textureLoader.load("/earth/day.jpg");
-earthDayTexture.colorSpace = THREE.SRGBColorSpace;
-earthDayTexture.anisotropy = 8;
-
-const earthNightTexture = textureLoader.load("/earth/night.jpg");
-earthNightTexture.colorSpace = THREE.SRGBColorSpace;
-earthNightTexture.anisotropy = 8;
-
-const earthSpecularCloudsTexture = textureLoader.load(
-  "/earth/specularClouds.jpg"
-);
-earthSpecularCloudsTexture.anisotropy = 8;
-
-// Mesh
-const earthGeometry = new THREE.SphereGeometry(2, 64, 64);
-const earthMaterial = new THREE.ShaderMaterial({
-  vertexShader: earthVertexShader,
-  fragmentShader: earthFragmentShader,
-  uniforms: {
-    uDayTexture: new THREE.Uniform(earthDayTexture),
-    uNightTexture: new THREE.Uniform(earthNightTexture),
-    uSpecularCloudsTexture: new THREE.Uniform(earthSpecularCloudsTexture),
-    uSunDirection: new THREE.Uniform(new THREE.Vector3(0, 0, 1)),
-    uAtmosphereDayColor: new THREE.Uniform(
-      new THREE.Color(earthParameters.atmosphereDayColor)
-    ),
-    uAtmosphereTwilightColor: new THREE.Uniform(
-      new THREE.Color(earthParameters.atmosphereTwilightColor)
-    ),
-  },
-});
-const earth = new THREE.Mesh(earthGeometry, earthMaterial);
-scene.add(earth);
-
-/**
- * Atmosphere
- */
-const atmosphereMaterial = new THREE.ShaderMaterial({
-  side: THREE.BackSide,
-  transparent: true,
-  vertexShader: atmosphereVertexShader,
-  fragmentShader: atmosphereFragmentShader,
-  uniforms: {
-    uSunDirection: new THREE.Uniform(new THREE.Vector3(0, 0, 1)),
-    uAtmosphereDayColor: new THREE.Uniform(
-      new THREE.Color(earthParameters.atmosphereDayColor)
-    ),
-    uAtmosphereTwilightColor: new THREE.Uniform(
-      new THREE.Color(earthParameters.atmosphereTwilightColor)
-    ),
-  },
-});
-
-const atmosphere = new THREE.Mesh(earthGeometry, atmosphereMaterial);
-atmosphere.scale.set(1.04, 1.04, 1.04);
-scene.add(atmosphere);
-
-/**
- * Sun
- */
-const sunSpherical = new THREE.Spherical(1, Math.PI * 0.5, 0.5);
-const sunDirection = new THREE.Vector3();
-
-// Debug
-const debugSun = new THREE.Mesh(
-  new THREE.IcosahedronGeometry(0.1, 2),
-  new THREE.MeshBasicMaterial()
-);
-scene.add(debugSun);
-
-// Update
-const updateSun = () => {
-  // Sun direction
-  sunDirection.setFromSpherical(sunSpherical);
-
-  // Debug
-  debugSun.position.copy(sunDirection).multiplyScalar(5);
-
-  // Uniforms
-  earthMaterial.uniforms.uSunDirection.value.copy(sunDirection);
-  atmosphereMaterial.uniforms.uSunDirection.value.copy(sunDirection);
-};
-updateSun();
-
-// Tweaks
-gui
-  .add(sunSpherical, "phi")
-  .min(0)
-  .max(Math.PI)
-  .step(0.01)
-  .name("sun phi")
-  .onChange(updateSun);
-
-gui
-  .add(sunSpherical, "theta")
-  .min(-Math.PI)
-  .max(Math.PI)
-  .step(0.01)
-  .name("sun theta")
-  .onChange(updateSun);
 
 /**
  * Sizes
@@ -169,6 +30,12 @@ window.addEventListener("resize", () => {
   sizes.height = window.innerHeight;
   sizes.pixelRatio = Math.min(window.devicePixelRatio, 2);
 
+  // Materials
+  particlesMaterial.uniforms.uResolution.value.set(
+    sizes.width * sizes.pixelRatio,
+    sizes.height * sizes.pixelRatio
+  );
+
   // Update camera
   camera.aspect = sizes.width / sizes.height;
   camera.updateProjectionMatrix();
@@ -183,14 +50,12 @@ window.addEventListener("resize", () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(
-  25,
+  35,
   sizes.width / sizes.height,
   0.1,
   100
 );
-camera.position.x = 12;
-camera.position.y = 5;
-camera.position.z = 4;
+camera.position.set(0, 0, 18);
 scene.add(camera);
 
 // Controls
@@ -204,20 +69,34 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
   antialias: true,
 });
+renderer.setClearColor("#181818");
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(sizes.pixelRatio);
-renderer.setClearColor("#000011");
+
+/**
+ * Particles
+ */
+const particlesGeometry = new THREE.PlaneGeometry(10, 10, 32, 32);
+
+const particlesMaterial = new THREE.ShaderMaterial({
+  vertexShader: particlesVertexShader,
+  fragmentShader: particlesFragmentShader,
+  uniforms: {
+    uResolution: new THREE.Uniform(
+      new THREE.Vector2(
+        sizes.width * sizes.pixelRatio,
+        sizes.height * sizes.pixelRatio
+      )
+    ),
+  },
+});
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
 
 /**
  * Animate
  */
-const clock = new THREE.Clock();
-
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-
-  earth.rotation.y = elapsedTime * 0.1;
-
   // Update controls
   controls.update();
 
