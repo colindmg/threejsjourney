3acf081d61122580a7e4b862b7894143f8c318e4
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-09-21 18:34:26 +0200

    Amélioration de la transition et animation du progress avec gsap

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import gsap from "gsap";
 import GUI from "lil-gui";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
@@ -98,6 +99,7 @@ let particles = null;
 
 gltfLoader.load("/models.glb", (gltf) => {
   particles = {};
+  particles.index = 0;
 
   // Positions
   const positions = gltf.scene.children.map(
@@ -135,7 +137,10 @@ gltfLoader.load("/models.glb", (gltf) => {
 
   // Geometry
   particles.geometry = new THREE.BufferGeometry();
-  particles.geometry.setAttribute("position", particles.positions[1]);
+  particles.geometry.setAttribute(
+    "position",
+    particles.positions[particles.index]
+  );
   particles.geometry.setAttribute("aPositionTarget", particles.positions[3]);
 
   // Material
@@ -160,13 +165,50 @@ gltfLoader.load("/models.glb", (gltf) => {
   particles.points = new THREE.Points(particles.geometry, particles.material);
   scene.add(particles.points);
 
+  // Methods
+  particles.morph = (index) => {
+    // Update attributes
+    particles.geometry.attributes.position =
+      particles.positions[particles.index];
+    particles.geometry.attributes.aPositionTarget = particles.positions[index];
+
+    // Animate uProgress
+    gsap.fromTo(
+      particles.material.uniforms.uProgress,
+      { value: 0 },
+      { value: 1, duration: 3, ease: "linear" }
+    );
+
+    // Save index
+    particles.index = index;
+  };
+
+  particles.morph0 = () => {
+    particles.morph(0);
+  };
+  particles.morph1 = () => {
+    particles.morph(1);
+  };
+  particles.morph2 = () => {
+    particles.morph(2);
+  };
+  particles.morph3 = () => {
+    particles.morph(3);
+  };
+
   // Tweaks
   gui
     .add(particles.material.uniforms.uProgress, "value")
     .min(0)
     .max(1)
     .step(0.001)
-    .name("uProgress");
+    .name("uProgress")
+    .listen();
+
+  gui.add(particles, "morph0");
+  gui.add(particles, "morph1");
+  gui.add(particles, "morph2");
+  gui.add(particles, "morph3");
 });
 
 /**
