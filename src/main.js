b527c69933a6dbda4d476649bc72fd318a208d0e
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-09-21 17:38:36 +0200

    Chargement des modèles 3D

--- src/main.js
+++ src/main.js
@@ -41,10 +41,12 @@ window.addEventListener("resize", () => {
   sizes.pixelRatio = Math.min(window.devicePixelRatio, 2);
 
   // Materials
-  particles.material.uniforms.uResolution.value.set(
-    sizes.width * sizes.pixelRatio,
-    sizes.height * sizes.pixelRatio
-  );
+  if (particles) {
+    particles.material.uniforms.uResolution.value.set(
+      sizes.width * sizes.pixelRatio,
+      sizes.height * sizes.pixelRatio
+    );
+  }
 
   // Update camera
   camera.aspect = sizes.width / sizes.height;
@@ -90,34 +92,38 @@ gui.addColor(debugObject, "clearColor").onChange(() => {
 renderer.setClearColor(debugObject.clearColor);
 
 /**
- * Particles
+ * Load models
  */
-const particles = {};
+let particles = null;
 
-// Geometry
-particles.geometry = new THREE.SphereGeometry(3);
-particles.geometry.setIndex(null);
+gltfLoader.load("/models.glb", (gltf) => {
+  particles = {};
 
-// Material
-particles.material = new THREE.ShaderMaterial({
-  vertexShader: particlesVertexShader,
-  fragmentShader: particlesFragmentShader,
-  uniforms: {
-    uSize: new THREE.Uniform(0.4),
-    uResolution: new THREE.Uniform(
-      new THREE.Vector2(
-        sizes.width * sizes.pixelRatio,
-        sizes.height * sizes.pixelRatio
-      )
-    ),
-  },
-  blending: THREE.AdditiveBlending,
-  depthWrite: false,
+  // Geometry
+  particles.geometry = new THREE.SphereGeometry(3);
+  particles.geometry.setIndex(null);
+
+  // Material
+  particles.material = new THREE.ShaderMaterial({
+    vertexShader: particlesVertexShader,
+    fragmentShader: particlesFragmentShader,
+    uniforms: {
+      uSize: new THREE.Uniform(0.4),
+      uResolution: new THREE.Uniform(
+        new THREE.Vector2(
+          sizes.width * sizes.pixelRatio,
+          sizes.height * sizes.pixelRatio
+        )
+      ),
+    },
+    blending: THREE.AdditiveBlending,
+    depthWrite: false,
+  });
+
+  // Points
+  particles.points = new THREE.Points(particles.geometry, particles.material);
+  scene.add(particles.points);
 });
-
-// Points
-particles.points = new THREE.Points(particles.geometry, particles.material);
-scene.add(particles.points);
 
 /**
  * Animate
