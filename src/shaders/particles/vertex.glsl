83d26b4a22cebd5916fb5a7e9e169d6cac64066e
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-10-02 17:55:18 +0200

    Life size, effet de pop-in/pop-out au début et à la fin de la vie d'une particule

--- src/shaders/particles/vertex.glsl
+++ src/shaders/particles/vertex.glsl
@@ -20,7 +20,11 @@ void main()
     gl_Position = projectedPosition;
 
     // Point size
-    gl_PointSize = aSize * uSize * uResolution.y;
+    float sizeIn = smoothstep(0.0, 0.1, particle.a);
+    float sizeOut = 1.0 - smoothstep(0.7, 1.0, particle.a);
+    float size = min(sizeIn, sizeOut);
+
+    gl_PointSize = size * aSize * uSize * uResolution.y;
     gl_PointSize *= (1.0 / - viewPosition.z);
 
     // Varyings
