19ea9a3f37340510c2fe1b9be61480072d395476
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-10-04 09:29:54 +0200

    Test du wobble en important un modèle

--- src/main.js
+++ src/main.js
@@ -121,17 +121,27 @@ gui.add(material, "transmission", 0, 1, 0.001);
 gui.add(material, "ior", 0, 10, 0.001);
 gui.add(material, "thickness", 0, 10, 0.001);
 
-// Geometry
-let geometry = new THREE.IcosahedronGeometry(2.5, 50);
-geometry = mergeVertices(geometry);
-geometry.computeTangents();
+// // Geometry
+// let geometry = new THREE.IcosahedronGeometry(2.5, 50);
+// geometry = mergeVertices(geometry);
+// geometry.computeTangents();
 
-// Mesh
-const wobble = new THREE.Mesh(geometry, material);
-wobble.customDepthMaterial = depthMaterial;
-wobble.receiveShadow = true;
-wobble.castShadow = true;
-scene.add(wobble);
+// // Mesh
+// const wobble = new THREE.Mesh(geometry, material);
+// wobble.customDepthMaterial = depthMaterial;
+// wobble.receiveShadow = true;
+// wobble.castShadow = true;
+// scene.add(wobble);
+
+// Load model
+gltfLoader.load("./suzanne.glb", (gltf) => {
+  const wobble = gltf.scene.children[0];
+  wobble.receiveShadow = true;
+  wobble.castShadow = true;
+  wobble.material = material;
+  wobble.customDepthMaterial = depthMaterial;
+  scene.add(wobble);
+});
 
 /**
  * Plane
