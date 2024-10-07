fefaf53aef2e99b8401add47cc7284138ac575b1
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-10-07 21:19:18 +0200

    Mise en place de l'EffectComposer et test du DotScreenPass

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import GUI from "lil-gui";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { DotScreenPass } from "three/examples/jsm/postprocessing/DotScreenPass.js";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
 
 /**
  * Base
@@ -130,6 +133,19 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
+ * Postprocessing
+ */
+const effectComposer = new EffectComposer(renderer);
+effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+effectComposer.setSize(sizes.width, sizes.height);
+
+const renderPass = new RenderPass(scene, camera);
+effectComposer.addPass(renderPass);
+
+const dotScreenPass = new DotScreenPass();
+effectComposer.addPass(dotScreenPass);
+
+/**
  * Animate
  */
 const clock = new THREE.Clock();
@@ -141,7 +157,8 @@ const tick = () => {
   controls.update();
 
   // Render
-  renderer.render(scene, camera);
+  // renderer.render(scene, camera);
+  effectComposer.render();
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick);
