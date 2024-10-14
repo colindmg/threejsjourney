2b9fdc9042a0feda10cf7b2b7320795723ed5463
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-10-14 22:05:55 +0200

    Utilisation d'un loading manager pour savoir si les éléments de la scène sont chargés

--- src/main.js
+++ src/main.js
@@ -5,8 +5,18 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 /**
  * Loaders
  */
-const gltfLoader = new GLTFLoader();
-const cubeTextureLoader = new THREE.CubeTextureLoader();
+const loadingManager = new THREE.LoadingManager(
+  // Loaded
+  () => {
+    console.log("loaded");
+  },
+  // Progress
+  (item, loaded, total) => {
+    console.log(item, loaded, total);
+  }
+);
+const gltfLoader = new GLTFLoader(loadingManager);
+const cubeTextureLoader = new THREE.CubeTextureLoader(loadingManager);
 
 /**
  * Base
@@ -38,7 +48,7 @@ const overlayMaterial = new THREE.ShaderMaterial({
   fragmentShader: `
 		uniform float uAlpha;
 		void main() {
-			gl_FragColor = vec4(1.0, 0.0, 0.0, uAlpha);
+			gl_FragColor = vec4(0.0, 0.0, 0.0, uAlpha);
 		}
 	`,
 });
