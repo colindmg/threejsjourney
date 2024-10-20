d050529e8f797bd261c7ae98d9ea88f13f1653f5
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-10-20 20:52:59 +0200

    Apparition/disparition des points quand ils passent derrière le modèle

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 /**
  * Loaders
  */
+let sceneReady = false;
 const loadingBarElement = document.querySelector(".loading-bar");
 const loadingManager = new THREE.LoadingManager(
   // Loaded
@@ -23,6 +24,10 @@ const loadingManager = new THREE.LoadingManager(
       loadingBarElement.classList.add("ended");
       loadingBarElement.style.transform = "";
     }, 500);
+
+    window.setTimeout(() => {
+      sceneReady = true;
+    }, 2500);
   },
 
   // Progress
@@ -126,10 +131,19 @@ gltfLoader.load("/models/DamagedHelmet/glTF/DamagedHelmet.gltf", (gltf) => {
 /**
  * Points of interest
  */
+const raycaster = new THREE.Raycaster();
 const points = [
   {
     position: new THREE.Vector3(1.55, 0.3, -0.6),
     element: document.querySelector(".point-0"),
+  },
+  {
+    position: new THREE.Vector3(0.5, 0.8, -1.6),
+    element: document.querySelector(".point-1"),
+  },
+  {
+    position: new THREE.Vector3(1.6, -1.3, -0.7),
+    element: document.querySelector(".point-2"),
   },
 ];
 
@@ -205,14 +219,32 @@ const tick = () => {
   controls.update();
 
   // Go through each point
-  for (const point of points) {
-    const screenPosition = point.position.clone();
-    screenPosition.project(camera);
+  if (sceneReady) {
+    for (const point of points) {
+      const screenPosition = point.position.clone();
+      screenPosition.project(camera);
 
-    const translateX = screenPosition.x * sizes.width * 0.5;
-    const translateY = -screenPosition.y * sizes.height * 0.5;
+      // Check if the point is visible and hide it if it's not
+      raycaster.setFromCamera(screenPosition, camera);
+      const intersects = raycaster.intersectObjects(scene.children, true);
+      if (intersects.length === 0) {
+        point.element.classList.add("visible");
+      } else {
+        const intersectionDistance = intersects[0].distance;
+        const pointDistance = point.position.distanceTo(camera.position);
+        if (intersectionDistance > pointDistance) {
+          point.element.classList.add("visible");
+        } else {
+          point.element.classList.remove("visible");
+        }
+      }
 
-    point.element.style.transform = `translateX(${translateX}px) translateY(${translateY}px)`;
+      // Position X and Y
+      const translateX = screenPosition.x * sizes.width * 0.5;
+      const translateY = -screenPosition.y * sizes.height * 0.5;
+
+      point.element.style.transform = `translateX(${translateX}px) translateY(${translateY}px)`;
+    }
   }
 
   // Render
