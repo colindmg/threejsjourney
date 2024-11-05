0450120e27d6391f3b5c776605edce3fe37e8b6a
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-11-05 23:11:31 +0100

    Starter de la leçon 51 - Importing and optimizing the portal scene

--- src/main.js
+++ src/main.js
@@ -1,50 +1,16 @@
-import { gsap } from "gsap";
+import GUI from "lil-gui";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-
-/**
- * Loaders
- */
-let sceneReady = false;
-const loadingBarElement = document.querySelector(".loading-bar");
-const loadingManager = new THREE.LoadingManager(
-  // Loaded
-  () => {
-    // Wait a little
-    window.setTimeout(() => {
-      // Animate overlay
-      gsap.to(overlayMaterial.uniforms.uAlpha, {
-        duration: 3,
-        value: 0,
-        delay: 1,
-      });
-
-      // Update loadingBarElement
-      loadingBarElement.classList.add("ended");
-      loadingBarElement.style.transform = "";
-    }, 500);
-
-    window.setTimeout(() => {
-      sceneReady = true;
-    }, 2500);
-  },
-
-  // Progress
-  (itemUrl, itemsLoaded, itemsTotal) => {
-    // Calculate the progress and update the loadingBarElement
-    const progressRatio = itemsLoaded / itemsTotal;
-    loadingBarElement.style.transform = `scaleX(${progressRatio})`;
-  }
-);
-const gltfLoader = new GLTFLoader(loadingManager);
-const cubeTextureLoader = new THREE.CubeTextureLoader(loadingManager);
 
 /**
  * Base
  */
 // Debug
-const debugObject = {};
+const gui = new GUI({
+  width: 400,
+});
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -53,110 +19,28 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 /**
- * Overlay
+ * Loaders
  */
-const overlayGeometry = new THREE.PlaneGeometry(2, 2, 1, 1);
-const overlayMaterial = new THREE.ShaderMaterial({
-  // wireframe: true,
-  transparent: true,
-  uniforms: {
-    uAlpha: { value: 1 },
-  },
-  vertexShader: `
-        void main()
-        {
-            gl_Position = vec4(position, 1.0);
-        }
-    `,
-  fragmentShader: `
-        uniform float uAlpha;
+// Texture loader
+const textureLoader = new THREE.TextureLoader();
 
-        void main()
-        {
-            gl_FragColor = vec4(0.0, 0.0, 0.0, uAlpha);
-        }
-    `,
-});
-const overlay = new THREE.Mesh(overlayGeometry, overlayMaterial);
-scene.add(overlay);
+// Draco loader
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath("draco/");
+
+// GLTF loader
+const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dracoLoader);
 
 /**
- * Update all materials
+ * Object
  */
-const updateAllMaterials = () => {
-  scene.traverse((child) => {
-    if (
-      child instanceof THREE.Mesh &&
-      child.material instanceof THREE.MeshStandardMaterial
-    ) {
-      // child.material.envMap = environmentMap
-      child.material.envMapIntensity = debugObject.envMapIntensity;
-      child.material.needsUpdate = true;
-      child.castShadow = true;
-      child.receiveShadow = true;
-    }
-  });
-};
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial()
+);
 
-/**
- * Environment map
- */
-const environmentMap = cubeTextureLoader.load([
-  "/textures/environmentMaps/0/px.jpg",
-  "/textures/environmentMaps/0/nx.jpg",
-  "/textures/environmentMaps/0/py.jpg",
-  "/textures/environmentMaps/0/ny.jpg",
-  "/textures/environmentMaps/0/pz.jpg",
-  "/textures/environmentMaps/0/nz.jpg",
-]);
-
-environmentMap.colorSpace = THREE.SRGBColorSpace;
-
-scene.background = environmentMap;
-scene.environment = environmentMap;
-
-debugObject.envMapIntensity = 2.5;
-
-/**
- * Models
- */
-gltfLoader.load("/models/DamagedHelmet/glTF/DamagedHelmet.gltf", (gltf) => {
-  gltf.scene.scale.set(2.5, 2.5, 2.5);
-  gltf.scene.rotation.y = Math.PI * 0.5;
-  scene.add(gltf.scene);
-
-  updateAllMaterials();
-});
-
-/**
- * Points of interest
- */
-const raycaster = new THREE.Raycaster();
-const points = [
-  {
-    position: new THREE.Vector3(1.55, 0.3, -0.6),
-    element: document.querySelector(".point-0"),
-  },
-  {
-    position: new THREE.Vector3(0.5, 0.8, -1.6),
-    element: document.querySelector(".point-1"),
-  },
-  {
-    position: new THREE.Vector3(1.6, -1.3, -0.7),
-    element: document.querySelector(".point-2"),
-  },
-];
-
-/**
- * Lights
- */
-const directionalLight = new THREE.DirectionalLight("#ffffff", 3);
-directionalLight.castShadow = true;
-directionalLight.shadow.camera.far = 15;
-directionalLight.shadow.mapSize.set(1024, 1024);
-directionalLight.shadow.normalBias = 0.05;
-directionalLight.position.set(0.25, 3, -2.25);
-scene.add(directionalLight);
+scene.add(cube);
 
 /**
  * Sizes
@@ -185,12 +69,14 @@ window.addEventListener("resize", () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(
-  75,
+  45,
   sizes.width / sizes.height,
   0.1,
   100
 );
-camera.position.set(4, 1, -4);
+camera.position.x = 4;
+camera.position.y = 2;
+camera.position.z = 4;
 scene.add(camera);
 
 // Controls
@@ -204,48 +90,19 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
   antialias: true,
 });
-renderer.toneMapping = THREE.ReinhardToneMapping;
-renderer.toneMappingExposure = 3;
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
  */
+const clock = new THREE.Clock();
+
 const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
   // Update controls
   controls.update();
-
-  // Go through each point
-  if (sceneReady) {
-    for (const point of points) {
-      const screenPosition = point.position.clone();
-      screenPosition.project(camera);
-
-      // Check if the point is visible and hide it if it's not
-      raycaster.setFromCamera(screenPosition, camera);
-      const intersects = raycaster.intersectObjects(scene.children, true);
-      if (intersects.length === 0) {
-        point.element.classList.add("visible");
-      } else {
-        const intersectionDistance = intersects[0].distance;
-        const pointDistance = point.position.distanceTo(camera.position);
-        if (intersectionDistance > pointDistance) {
-          point.element.classList.add("visible");
-        } else {
-          point.element.classList.remove("visible");
-        }
-      }
-
-      // Position X and Y
-      const translateX = screenPosition.x * sizes.width * 0.5;
-      const translateY = -screenPosition.y * sizes.height * 0.5;
-
-      point.element.style.transform = `translateX(${translateX}px) translateY(${translateY}px)`;
-    }
-  }
 
   // Render
   renderer.render(scene, camera);
