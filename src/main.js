457a240f2d47438ebaf9734477985740fb5c778b
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-11-06 20:04:53 +0100

    Fix des couleurs (ColorSPace de la texture)

--- src/main.js
+++ src/main.js
@@ -37,6 +37,7 @@ gltfLoader.setDRACOLoader(dracoLoader);
  */
 const bakedTexture = textureLoader.load("baked.jpg");
 bakedTexture.flipY = false;
+bakedTexture.colorSpace = THREE.SRGBColorSpace;
 
 /**
  * Materials
