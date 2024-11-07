421a0be5da6bf51302e3112b957eacd3747f4504
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-11-07 17:08:12 +0100

    Test de différents controls avec R3Drei

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
 import { Canvas } from "@react-three/fiber";
-import * as THREE from "three";
 import Experience from "./components/Experience";
 
 function App() {
-  const cameraSettings = {
-    fov: 45,
-    // zoom: 100,
-    near: 0.2,
-    far: 20,
-    position: [3, 2, 6],
-  };
-
   return (
     <>
-      <Canvas
-        // flat
-        // dpr={[1, 2]}
-        gl={{
-          antialias: true,
-          toneMapping: THREE.ACESFilmicToneMapping,
-          outputColorSpace: THREE.SRGBColorSpace,
-        }}
-        style={{ height: "100dvh", width: "100vw", background: "lightblue" }}
-        // orthographic
-        camera={cameraSettings}
-      >
+      <Canvas style={{ height: "100dvh", width: "100vw" }}>
         <Experience />
       </Canvas>
     </>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,58 +1,46 @@
-import { useFrame } from "@react-three/fiber";
+import {
+  OrbitControls,
+  PivotControls,
+  TransformControls,
+} from "@react-three/drei";
 import { useRef } from "react";
-import CustomObject from "./CustomObject";
 
 const Experience = () => {
   // REFS
-  const groupRef = useRef();
   const cubeRef = useRef();
-
-  // ANIMATIONS
-  useFrame((state, delta) => {
-    cubeRef.current.rotation.y += delta;
-    // groupRef.current.rotation.y += delta;
-
-    // Animation of the camera
-    const angle = state.clock.getElapsedTime();
-    state.camera.position.x = Math.cos(angle * 0.3) * 8;
-    state.camera.position.z = Math.sin(angle * 0.3) * 8;
-    state.camera.lookAt(0, 0, 0);
-  });
 
   return (
     <>
       {/* CONTROLS */}
-      {/* <OrbitControls /> */}
+      <OrbitControls makeDefault />
 
       {/* LIGHTS */}
       <directionalLight position={[5, 5, 5]} intensity={4.5} />
       <ambientLight intensity={1.5} />
 
       {/* MESHES */}
-      <group ref={groupRef}>
+      <PivotControls anchor={[0, 0, 0]} depthTest={false}>
         <mesh position-x={-2}>
           <sphereGeometry />
           <meshStandardMaterial color="orange" />
         </mesh>
+      </PivotControls>
 
-        <mesh
-          ref={cubeRef}
-          rotation-y={Math.PI * 0.25}
-          position-x={2}
-          scale={1.5}
-        >
-          <boxGeometry />
-          <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-      </group>
+      <mesh
+        ref={cubeRef}
+        rotation-y={Math.PI * 0.25}
+        position-x={2}
+        scale={1.5}
+      >
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+      <TransformControls object={cubeRef} />
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>
-
-      {/* CUSTOM OBJECT */}
-      <CustomObject />
     </>
   );
 };
