8e0c97f37393a9094cbb42342f3956658eefdecd
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-11-07 08:41:19 +0100

    Scène basique en R3F

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Canvas } from "@react-three/fiber";
+import Experience from "./components/Experience";
 
 function App() {
   return (
     <>
       <Canvas style={{ height: "100dvh", width: "100vw" }}>
-        <mesh>
-          <boxGeometry />
-          <meshBasicMaterial color="red" />
-        </mesh>
+        <Experience />
       </Canvas>
     </>
   );
---
+++ src/components/Experience.jsx
@@ -0,0 +1,51 @@
+import { OrbitControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+const Experience = () => {
+  // REFS
+  const groupRef = useRef();
+  const cubeRef = useRef();
+
+  // ANIMATIONS
+  useFrame((state, delta) => {
+    cubeRef.current.rotation.y += delta;
+    groupRef.current.rotation.y += delta;
+  });
+
+  return (
+    <>
+      {/* CONTROLS */}
+      <OrbitControls />
+
+      {/* LIGHTS */}
+      <directionalLight position={[5, 5, 5]} intensity={4.5} />
+      <ambientLight intensity={1.5} />
+
+      {/* MESHES */}
+      <group ref={groupRef}>
+        <mesh position-x={-2}>
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
+        </mesh>
+
+        <mesh
+          ref={cubeRef}
+          rotation-y={Math.PI * 0.25}
+          position-x={2}
+          scale={1.5}
+        >
+          <boxGeometry />
+          <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+      </group>
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
+    </>
+  );
+};
+
+export default Experience;
