5cf0944a7afbc6bf83416a098dcb4f4796866aea
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-11-09 22:13:56 +0100

    Test de différentes shadows avec R3Drei

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls, useHelper } from "@react-three/drei";
+import { ContactShadows, OrbitControls, useHelper } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import * as THREE from "three";
@@ -9,7 +9,7 @@ const Experience = () => {
   const sphereRef = useRef();
   const dirLightRef = useRef();
 
-  useHelper(dirLightRef, THREE.DirectionalLightHelper, 2);
+  useHelper(dirLightRef, THREE.DirectionalLightHelper, 2, "red");
 
   // ANIMATION
   useFrame((state, delta) => {
@@ -24,17 +24,59 @@ const Experience = () => {
       {/* CONTROLS */}
       <OrbitControls makeDefault />
 
+      {/* HELPERS */}
+      {/* <BakeShadows /> */}
+      {/* <SoftShadows size={25} samples={10} focus={0} /> */}
+      {/* <AccumulativeShadows
+        position-y={-0.99}
+        color="#316D39"
+        opacity={0.8}
+        frames={Infinity}
+        blend={100}
+        temporal
+      >
+        <RandomizedLight
+          amount={8}
+          radius={1}
+          ambient={0.5}
+          intensity={3}
+          position={[1, 2, 3]}
+          bias={0.001}
+        />
+      </AccumulativeShadows> */}
+      <ContactShadows
+        position-y={-0.99}
+        resolution={512}
+        far={5}
+        scale={10}
+        blur={2}
+        // frames={1}
+      />
+
       {/* LIGHTS */}
-      <directionalLight ref={dirLightRef} position={[1, 2, 3]} intensity={3} />
+      <directionalLight
+        ref={dirLightRef}
+        position={[1, 2, 3]}
+        intensity={3}
+        castShadow
+        shadow-mapSize={[1024, 1024]}
+        shadow-camera-near={1}
+        shadow-camera-far={10}
+        shadow-camera-top={5}
+        shadow-camera-bottom={-5}
+        shadow-camera-right={5}
+        shadow-camera-left={-5}
+      />
       <ambientLight intensity={1.5} />
 
       {/* MESHES */}
-      <mesh ref={sphereRef} position-x={-2}>
+      <mesh castShadow ref={sphereRef} position-x={-2}>
         <sphereGeometry />
         <meshStandardMaterial color={"orange"} />
       </mesh>
 
       <mesh
+        castShadow
         ref={cubeRef}
         rotation-y={Math.PI * 0.25}
         position-x={2}
