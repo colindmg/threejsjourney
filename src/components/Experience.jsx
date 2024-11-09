40d69c8bc91691fe978830a069187bbc78e1bbf4
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-11-09 21:32:50 +0100

    Starter de la leçon 58 - Environment & staging

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,5 @@
 import { OrbitControls } from "@react-three/drei";
-import { button, useControls } from "leva";
-import { Perf } from "r3f-perf";
+import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 
 const Experience = () => {
@@ -8,34 +7,12 @@ const Experience = () => {
   const cubeRef = useRef();
   const sphereRef = useRef();
 
-  // LEVA CONTROLS
-  const { perfVisible } = useControls("Performance Monitor", {
-    perfVisible: true,
+  useFrame((state, delta) => {
+    cubeRef.current.rotation.y += delta * 0.2;
   });
-
-  const { position, visible, color } = useControls("Sphere", {
-    position: {
-      value: { x: -2, y: 0 },
-      step: 0.01,
-      joystick: "invertY",
-    },
-    visible: true,
-    color: "orange",
-  });
-
-  // Other useful tweaks exemples
-  const { interval, buttonTweak, choice } = useControls("Tweaks Exemple", {
-    interval: { value: [0, 10], min: 0, max: 100, step: 1 }, // Slider with interval
-    buttonTweak: button(() => console.log("Button clicked")), // Button
-    choice: { options: ["Option 1", "Option 2"], value: "Option 1" }, // Dropdown
-  });
-  console.log(interval, buttonTweak, choice);
 
   return (
     <>
-      {/* PERFORMANCE MONITOR */}
-      {perfVisible && <Perf position="top-left" />}
-
       {/* CONTROLS */}
       <OrbitControls makeDefault />
 
@@ -44,13 +21,9 @@ const Experience = () => {
       <ambientLight intensity={1.5} />
 
       {/* MESHES */}
-      <mesh
-        visible={visible}
-        ref={sphereRef}
-        position={[position.x, position.y, 0]}
-      >
+      <mesh ref={sphereRef} position-x={-2}>
         <sphereGeometry />
-        <meshStandardMaterial color={color} />
+        <meshStandardMaterial color={"orange"} />
       </mesh>
 
       <mesh
