51cb24270e6592fabe443a54673f602bb93c995a
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-11-10 13:12:16 +0100

    Ajout de donuts et optimisations geometry/material

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,13 +4,33 @@ import {
   Text3D,
   useMatcapTexture,
 } from "@react-three/drei";
+import { useEffect } from "react";
+import * as THREE from "three";
+
+const torusGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32);
+const matcapMaterial = new THREE.MeshMatcapMaterial();
 
 export default function Experience() {
+  // STORING GEOMETRY AND MATERIAL
+  // const [torusGeometry, setTorusGeometry] = useState();
+  // const [matcapMaterial, setMatcapMaterial] = useState();
+
+  // MATCAP TEXTURE
   const [matcapTexture] = useMatcapTexture("8B892C_D4E856_475E2D_47360A", 256);
   console.log(matcapTexture);
 
+  useEffect(() => {
+    matcapTexture.colorSpace = THREE.SRGBColorSpace;
+    matcapTexture.needsUpdate = true;
+    matcapMaterial.matcap = matcapTexture;
+    matcapMaterial.needsUpdate = true;
+  }, [matcapTexture]);
+
   return (
     <>
+      {/* <torusGeometry ref={setTorusGeometry} />
+      <meshMatcapMaterial ref={setMatcapMaterial} matcap={matcapTexture} /> */}
+
       {/* CONTROLS */}
       <OrbitControls makeDefault />
 
@@ -26,11 +46,27 @@ export default function Experience() {
           bevelSize={0.02}
           bevelOffset={0}
           bevelSegments={5}
+          material={matcapMaterial}
         >
-          Hello, World!
-          <meshMatcapMaterial matcap={matcapTexture} />
+          Thibaut Lecrazy
         </Text3D>
       </Center>
+
+      {/* DONUTS */}
+      {[...Array(100)].map((donut, index) => (
+        <mesh
+          key={"Donut number " + index}
+          position={[
+            (Math.random() - 0.5) * 10,
+            (Math.random() - 0.5) * 10,
+            (Math.random() - 0.5) * 10,
+          ]}
+          scale={0.2 + Math.random() * 0.2}
+          rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
+          geometry={torusGeometry}
+          material={matcapMaterial}
+        ></mesh>
+      ))}
     </>
   );
 }
