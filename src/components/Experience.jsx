063e24b9d544eeb2748fc79da4173ee4befac478
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-11-11 19:11:08 +0100

    Starter de la leçon 63 - Postprocessing with R3F

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,67 +1,39 @@
-import { meshBounds, OrbitControls, useGLTF } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
-import { useRef } from "react";
+import { OrbitControls } from "@react-three/drei";
+import { Perf } from "r3f-perf";
 
 export default function Experience() {
-  const cube = useRef();
-  const hamburger = useGLTF("/hamburger.glb");
-
-  useFrame((state, delta) => {
-    cube.current.rotation.y += delta * 0.2;
-  });
-
-  const eventHandler = (event) => {
-    console.log(event);
-    cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 50%)`);
-  };
-
   return (
     <>
+      {/* PERFS */}
+      <Perf position="top-left" />
+
       {/* CONTROLS */}
       <OrbitControls makeDefault />
 
       {/* LIGHTS */}
-      <directionalLight position={[1, 2, 3]} intensity={4.5} />
+      <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
       <ambientLight intensity={1.5} />
 
       {/* OBJECTS */}
-      <mesh
-        position-x={-2}
-        onClick={(e) => {
-          e.stopPropagation();
-        }}
-      >
+      <mesh castShadow position-x={-2}>
         <sphereGeometry />
         <meshStandardMaterial color="orange" />
       </mesh>
 
-      <mesh
-        ref={cube}
-        raycast={meshBounds}
-        position-x={2}
-        scale={1.5}
-        onClick={eventHandler}
-        onPointerEnter={() => (document.body.style.cursor = "pointer")}
-        onPointerLeave={() => (document.body.style.cursor = "auto")}
-      >
+      <mesh castShadow position-x={2} scale={1.5}>
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
 
-      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+      <mesh
+        receiveShadow
+        position-y={-1}
+        rotation-x={-Math.PI * 0.5}
+        scale={10}
+      >
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>
-
-      <primitive
-        object={hamburger.scene}
-        scale={0.25}
-        position-y={0.5}
-        onClick={(e) => {
-          e.stopPropagation();
-          console.log(e.object.name);
-        }}
-      />
     </>
   );
 }
