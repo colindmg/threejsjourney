d75ae84753c9cd1eca5002a3ba82a940c402e0a4
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-11-11 20:18:29 +0100

    Vignette effect et fix du background

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,14 +1,26 @@
 import { OrbitControls } from "@react-three/drei";
-import { EffectComposer, ToneMapping } from "@react-three/postprocessing";
-import { ToneMappingMode } from "postprocessing";
+import {
+  EffectComposer,
+  ToneMapping,
+  Vignette,
+} from "@react-three/postprocessing";
+import { BlendFunction, ToneMappingMode } from "postprocessing";
 import { Perf } from "r3f-perf";
 
 export default function Experience() {
   return (
     <>
+      {/* BACKGROUND COLOR */}
+      <color attach="background" args={["#FFF"]} />
+
       {/* POST-PROCESSING */}
       <EffectComposer multisampling={8}>
         <ToneMapping mode={ToneMappingMode.ACES_FILMIC} />
+        <Vignette
+          offset={0.3}
+          darkness={0.9}
+          blendFunction={BlendFunction.NORMAL}
+        />
       </EffectComposer>
 
       {/* PERFS */}
