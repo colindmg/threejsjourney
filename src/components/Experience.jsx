61634db8b250672610226bafb52ebd9fd48f64f5
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-11-12 21:27:04 +0100

    Ajout de l'iframe et d'une light

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,6 +2,7 @@ import {
   ContactShadows,
   Environment,
   Float,
+  Html,
   PresentationControls,
   useGLTF,
 } from "@react-three/drei";
@@ -27,7 +28,26 @@ export default function Experience() {
       >
         {/* OBJECT */}
         <Float rotationIntensity={0.4}>
-          <primitive position-y={-1.2} object={computer.scene} />
+          <rectAreaLight
+            width={2.5}
+            height={1.65}
+            intensity={65}
+            color={"#ff6900"}
+            rotation={[-0.1, Math.PI, 0]}
+            position={[0, 0.55, -1.15]}
+          />
+
+          <primitive position-y={-1.2} object={computer.scene}>
+            <Html
+              transform
+              wrapperClass="htmlScreen"
+              distanceFactor={1.17}
+              position={[0, 1.56, -1.4]}
+              rotation-x={-0.256}
+            >
+              <iframe src="https://bruno-simon.com/html" />
+            </Html>
+          </primitive>
         </Float>
       </PresentationControls>
 
