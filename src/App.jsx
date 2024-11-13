cecc398f58c872ecf20b0ef4f3600ce7a2d075ea
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-11-13 20:40:21 +0100

    Starter de la leçon 65 - Physics w/ R3F

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Experience from "./components/Experience";
 function App() {
   return (
     <>
-      <Canvas style={{ height: "100dvh", width: "100vw", touchAction: "none" }}>
+      <Canvas style={{ height: "100dvh", width: "100vw" }}>
         <Experience />
       </Canvas>
     </>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,68 +1,34 @@
-import {
-  ContactShadows,
-  Environment,
-  Float,
-  Html,
-  PresentationControls,
-  Text,
-  useGLTF,
-} from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
+import { Perf } from "r3f-perf";
 
 export default function Experience() {
-  const computer = useGLTF("/macbook.gltf");
-
   return (
     <>
-      {/* BACKGROUND COLOR */}
-      <color attach="background" args={["#241A1A"]} />
+      {/* PERFS */}
+      <Perf position="top-left" />
 
-      {/* ENVIRONMENT */}
-      <Environment preset="city" />
+      {/* CONTROLS */}
+      <OrbitControls makeDefault />
 
-      <PresentationControls
-        global
-        rotation={[0.13, 0.1, 0]}
-        polar={[-0.4, 0.2]}
-        azimuth={[-1, 0.75]}
-        config={{ mass: 2, tension: 400 }}
-        snap={{ mass: 4, tension: 400 }}
-      >
-        {/* OBJECT */}
-        <Float rotationIntensity={0.4}>
-          <rectAreaLight
-            width={2.5}
-            height={1.65}
-            intensity={65}
-            color={"#ff6900"}
-            rotation={[-0.1, Math.PI, 0]}
-            position={[0, 0.55, -1.15]}
-          />
+      {/* LIGHTS */}
+      <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
+      <ambientLight intensity={1.5} />
 
-          <primitive position-y={-1.2} object={computer.scene}>
-            <Html
-              transform
-              wrapperClass="htmlScreen"
-              distanceFactor={1.17}
-              position={[0, 1.56, -1.4]}
-              rotation-x={-0.256}
-            >
-              <iframe src="https://bruno-simon.com/html" />
-            </Html>
-          </primitive>
-          <Text
-            font="/bangers.woff"
-            fontSize={1}
-            position={[2, 0.75, 0.75]}
-            rotation-y={-1.25}
-            maxWidth={2}
-            textAlign="center"
-          >
-            BRUNO SIMON
-          </Text>
-        </Float>
-      </PresentationControls>
+      {/* OBJECTS */}
+      <mesh castShadow position={[-2, 2, 0]}>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
 
-      <ContactShadows position-y={-1.4} opacity={0.4} scale={5} blur={2.4} />
+      <mesh castShadow position={[2, 2, 0]}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+
+      <mesh receiveShadow position-y={-1.25}>
+        <boxGeometry args={[10, 0.5, 10]} />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
     </>
   );
 }
