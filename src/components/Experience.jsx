83d2d12c80c26a300bb0c0542c2a0a5dfbc8e9c7
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-11-14 20:28:30 +0100

    Stress test en utilisant des instanced mesh

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,14 +3,16 @@ import { useFrame } from "@react-three/fiber";
 import {
   CuboidCollider,
   CylinderCollider,
+  InstancedRigidBodies,
   Physics,
   RigidBody,
 } from "@react-three/rapier";
 import { Perf } from "r3f-perf";
-import { useRef, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 import * as THREE from "three";
 
 export default function Experience() {
+  // MODEL
   const hamburger = useGLTF("/hamburger.glb");
 
   // STATES
@@ -42,7 +44,7 @@ export default function Experience() {
   useFrame((state) => {
     const time = state.clock.getElapsedTime();
 
-    const eulerRotation = new THREE.Euler(0, time * 3, 0);
+    const eulerRotation = new THREE.Euler(0, time * 4, 0);
     const quaternionRotation = new THREE.Quaternion();
     quaternionRotation.setFromEuler(eulerRotation);
 
@@ -53,6 +55,39 @@ export default function Experience() {
     const z = Math.sin(angle) * 2;
     twister.current.setNextKinematicTranslation({ x, y: 0.5, z: z });
   });
+
+  // STRESS TEST
+  const cubeCount = 100;
+  // const cubes = useRef();
+
+  // useEffect(() => {
+  //   for (let i = 0; i < cubeCount; i++) {
+  //     const matrix = new THREE.Matrix4();
+  //     matrix.compose(
+  //       new THREE.Vector3(i * 2, 2, 0),
+  //       new THREE.Quaternion(),
+  //       new THREE.Vector3(1, 1, 1)
+  //     );
+  //     cubes.current.setMatrixAt(i, matrix);
+  //   }
+  // }, []);
+  const instances = useMemo(() => {
+    const instances = [];
+
+    for (let i = 0; i < cubeCount; i++) {
+      instances.push({
+        key: "instance_" + i,
+        position: [
+          (Math.random() - 0.5) * 8,
+          6 + i * 0.2,
+          (Math.random() - 0.5) * 8,
+        ],
+        rotation: [Math.random(), Math.random(), Math.random()],
+      });
+    }
+
+    return instances;
+  }, []);
 
   return (
     <>
@@ -67,7 +102,7 @@ export default function Experience() {
       <ambientLight intensity={1.5} />
 
       {/* OBJECTS */}
-      <Physics debug gravity={[0, -9.08, 0]}>
+      <Physics debug={false} gravity={[0, -9.08, 0]}>
         <RigidBody colliders="ball">
           <mesh castShadow position={[-2, 2, 0]}>
             <sphereGeometry />
@@ -114,11 +149,31 @@ export default function Experience() {
           </mesh>
         </RigidBody>
 
+        {/* INVISIBLE WALLS */}
+        <RigidBody type="fixed">
+          <CuboidCollider args={[5, 2, 0.5]} position={[0, 1, 5.5]} />
+          <CuboidCollider args={[5, 2, 0.5]} position={[0, 1, -5.5]} />
+          <CuboidCollider args={[0.5, 2, 5]} position={[5.5, 1, 0]} />
+          <CuboidCollider args={[0.5, 2, 5]} position={[-5.5, 1, 0]} />
+        </RigidBody>
+
         {/* HAMBURGER */}
         <RigidBody colliders={false} position={[0, 4, 0]}>
           <CylinderCollider mass={1} args={[0.5, 1.25]} />
           <primitive object={hamburger.scene} scale={0.25} />
         </RigidBody>
+
+        {/* STRESS TEST */}
+        <InstancedRigidBodies instances={instances}>
+          <instancedMesh
+            //ref={cubes}
+            args={[null, null, cubeCount]}
+            castShadow
+          >
+            <boxGeometry />
+            <meshStandardMaterial color={"tomato"} />
+          </instancedMesh>
+        </InstancedRigidBodies>
       </Physics>
     </>
   );
